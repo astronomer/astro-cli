27110a0ec5023035dd4797530f83c30ec3f15a2f
schnie <greg@astronomer.io>
2018-02-13 21:29:50 -0500

    Return output from docker commands

--- docker/docker.go
+++ docker/docker.go
@@ -13,13 +13,16 @@ const (
 )
 
 // Exec executes a docker command
-func Exec(args ...string) {
+func Exec(args ...string) string {
 	_, lookErr := exec.LookPath(Docker)
 	if lookErr != nil {
 		panic(lookErr)
 	}
 
-	if cmdErr := exec.Command(Docker, args...).Run(); cmdErr != nil {
+	output, cmdErr := exec.Command(Docker, args...).Output()
+	if cmdErr != nil {
 		panic(cmdErr)
 	}
+
+	return string(output)
 }
