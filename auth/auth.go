855c3612ef076eef39478e09d7d3705968cb3a30
Andy Cooper <andycooper.s@gmail.com>
2018-04-17 15:28:15 -0400

    Capture and pass auth to docker login
    This commit allows for us to capture credentials in the same command we auth to the private registry. It paves the way for a single auth command to login to houston and the registry

--- auth/auth.go
+++ auth/auth.go
@@ -1,13 +1,30 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/astronomerio/astro-cli/config"
 	"github.com/astronomerio/astro-cli/docker"
+	"github.com/astronomerio/astro-cli/utils"
 )
 
 // Login logs a user into the docker registry. Will need to login to Houston next.
 func Login() {
-	docker.Exec("login", config.CFG.RegistryAuthority.GetString())
+	registry := config.CFG.RegistryAuthority.GetString()
+	username := utils.InputText("Username: ")
+	password, _ := utils.InputPassword("Password: ")
+
+	err := docker.ExecLogin(registry, username, password)
+	if err != nil {
+		// Println instead of panic to prevent excessive error logging to
+		// stdout on a failed login
+		fmt.Println(err)
+		return
+	}
+
+	// pass successful credentials to config
+	config.CFG.RegistryUser.SetProjectString(username)
+	config.CFG.RegistryPassword.SetProjectString(password)
 }
 
 // Logout logs a user out of the docker registry. Will need to logout of Houston next.
--- docker/docker.go
+++ docker/docker.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -25,4 +26,22 @@ func Exec(args ...string) {
 	if cmdErr := cmd.Run(); cmdErr != nil {
 		panic(cmdErr)
 	}
+}
+
+// ExecLogin executes a docker login
+// Is a workaround for submitting password to stdin without prompting user again
+func ExecLogin(registry, username, password string) error {
+	_, lookErr := exec.LookPath(Docker)
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	loginCmd := fmt.Sprintf("echo '%s' | docker login %s -u %s --password-stdin", password, registry, username)
+	cmd := exec.Command("bash", "-c", loginCmd)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
