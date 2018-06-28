6a1355c6bf962e67200aa43c49af56cb241ed8a5
Andy Cooper <andycooper.s@gmail.com>
2018-06-28 11:48:59 -0400

    Build out upgrade check logic

--- cmd/version.go
+++ cmd/version.go
@@ -2,15 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/astronomerio/astro-cli/pkg/github"
-	"github.com/astronomerio/astro-cli/pkg/httputil"
 	"github.com/spf13/cobra"
 )
 
 var (
-	http       = httputil.NewHTTPClient()
 	version    string
 	gitCommit  string
 	versionCmd = &cobra.Command{
@@ -34,7 +31,7 @@ func init() {
 }
 
 func printVersion(cmd *cobra.Command, args []string) error {
-	fmt.Printf("Astro CLI Version: %s\n", version)
+	fmt.Printf("Astro CLI Version: v%s\n", version)
 	fmt.Printf("Git Commit: %s\n", gitCommit)
 	return nil
 }
@@ -42,19 +39,34 @@ func printVersion(cmd *cobra.Command, args []string) error {
 func upgradeCheck(cmd *cobra.Command, args []string) error {
 	if len(version) == 0 {
 		fmt.Println(`Your current Astronomer CLI is not bound to a git commit.
-This is likely the result of building from source. You can install the latest tagged release with the following command
+	This is likely the result of building from source. You can install the latest tagged release with the following command
+
+		$ curl -sL https://install.astronomer.io | sudo bash`)
+		return nil
+	}
+
+	repoLatestTag, err := github.RepoLatestRequest("astronomerio", "astro-cli")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	currentTag, err := github.RepoTagRequest("astronomerio", "astro-cli", string("v")+version)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	currentPub := currentTag.PublishedAt.Format("2006.01.02")
+	latestPub := repoLatestTag.PublishedAt.Format("2006.01.02")
+
+	if repoLatestTag.TagName > version {
+		fmt.Printf("Astro CLI Version: v%s (%s)\n", version, currentPub)
+		fmt.Printf("Astro CLI Latest: %s (%s)\n", repoLatestTag.TagName, latestPub)
+		fmt.Println(`There is a more recent version of the Astronomer CLI available.
+You can install the latest tagged release with the following command
 
 	$ curl -sL https://install.astronomer.io | sudo bash`)
 		return nil
 	}
 
-	api := github.NewGithubClient(http)
-	repoLatest, err := api.RepoLatestRequest("astronomerio", "astro-cli")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(repoLatest.TagName)
 	return nil
 }
--- pkg/github/github.go
+++ pkg/github/github.go
@@ -5,38 +5,47 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"time"
 
 	"github.com/astronomerio/astro-cli/pkg/httputil"
 	"github.com/pkg/errors"
 )
 
+var (
+	http = httputil.NewHTTPClient()
+	api  = NewGithubClient(http)
+)
+
+// RepoLatestResponse represents a tag info response from Github API
 type RepoLatestResponse struct {
-	Url     string `json:"url"`
-	TagName string `json:"tag_name"`
-	Draft   bool   `json:"draft"`
-	// TODO created_at
+	Url         string    `json:"url"`
+	TagName     string    `json:"tag_name"`
+	Draft       bool      `json:"draft"`
+	CreatedAt   time.Time `json:"created_at"`
+	PublishedAt time.Time `json:"published_at"`
 }
 
 type Client struct {
 	HTTPClient *httputil.HTTPClient
 }
 
+// NewGithubClient returns a HTTP client for interfacing with github
 func NewGithubClient(c *httputil.HTTPClient) *Client {
 	return &Client{
 		HTTPClient: c,
 	}
 }
 
-func (c *Client) RepoLatestRequest(orgName string, repoName string) (*RepoLatestResponse, error) {
+// GithubRequest Sends an http request to github and returns a basic response obj
+func (c *Client) GithubRequest(url string, method string) (*httputil.HTTPResponse, error) {
 	doOpts := httputil.DoOptions{
 		Headers: map[string]string{
 			"Accept": "application/json",
 		},
 	}
 
-	url := "https://api.github.com/repos/%s/%s/releases/latest"
 	var response httputil.HTTPResponse
-	httpResponse, err := c.HTTPClient.Do("GET", fmt.Sprintf(url, orgName, repoName), &doOpts)
+	httpResponse, err := c.HTTPClient.Do(method, url, &doOpts)
 	if err != nil {
 		return nil, err
 	}
@@ -53,11 +62,41 @@ func (c *Client) RepoLatestRequest(orgName string, repoName string) (*RepoLatest
 		Body: string(body),
 	}
 
+	return &response, nil
+}
+
+// RepoLatestRequest Makes a request to grab the latest release of a github repository
+func RepoLatestRequest(orgName string, repoName string) (*RepoLatestResponse, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", orgName, repoName)
+
+	response, err := api.GithubRequest(url, "GET")
+	if err != nil {
+		return nil, err
+	}
+
 	decode := RepoLatestResponse{}
 	err = json.NewDecoder(strings.NewReader(response.Body)).Decode(&decode)
 	if err != nil {
 		//logger.Error(err)
-		return nil, errors.Wrap(err, "Failed to JSON decode Github response")
+		return nil, errors.Wrap(err, fmt.Sprintf("Failed to JSON decode Github response from %s", url))
+	}
+	return &decode, nil
+}
+
+// RepoTagRequest makes a request to grab a specific tag of a github repository
+func RepoTagRequest(orgName string, repoName string, tagName string) (*RepoLatestResponse, error) {
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/tags/%s", orgName, repoName, tagName)
+	fmt.Println(url)
+	response, err := api.GithubRequest(url, "GET")
+	if err != nil {
+		return nil, err
+	}
+
+	decode := RepoLatestResponse{}
+	err = json.NewDecoder(strings.NewReader(response.Body)).Decode(&decode)
+	if err != nil {
+		//logger.Error(err)
+		return nil, errors.Wrap(err, fmt.Sprintf("Failed to JSON decode Github response from %s", url))
 	}
 	return &decode, nil
 }
