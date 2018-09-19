132fb764e59d71e0419a96f60372263aedc3937f
Andy Cooper <andycooper.s@gmail.com>
2018-09-19 16:36:20 -0400

    Disable circle-ci autobuilds

--- .circleci/scripts/write_metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=$1
-BUILD_DATE="`date '+%B %d, %Y @ %H:%M:%S'`"
-CONTENT="package meta\n\nvar VERSION = \"$VERSION\"\nvar BUILD_DATE = \"$BUILD_DATE\""
-
-mkdir -p meta
-
-printf "$CONTENT" > ./meta/meta.go
