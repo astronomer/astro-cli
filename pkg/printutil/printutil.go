4e924d49a086fed1213ce9bc5b79bd4da5f3b9fd
Andy Cooper <andycooper.s@gmail.com>
2018-09-24 10:59:28 -0400

    Refactor tab print for color printing

--- pkg/printutil/printutil.go
+++ pkg/printutil/printutil.go
@@ -5,16 +5,64 @@ import (
 )
 
 type Table struct {
-	Padding      []int
-	Header       []string
-	Rows         [][]string
-	RenderedRows []string
-	// ColorCond    func() bool
-	SuccessMsg   string
+	// A slice of ints defining the padding for each column
+	Padding         []int
+	RenderedPadding string
+
+	// Slice of strings representing column headers
+	Header         []string
+	RenderedHeader string
+
+	// Truncate rows if they exceed padding length
+	Truncate bool
+	// 2d slice of strings, with each slice containing a row
+	// representation (nested slice of strings) of info to print
+	// Rows [][]string
+
+	// A place to store rows with padding correctly applied to them
+	Rows []Row
+
+	// A message to print after table has been printed
+	SuccessMsg string
+
+	// Optional message to print if no rows were passed to table
 	NoResultsMsg string
+
+	// Highlight a row with color
+
+	// Function which will eval whether to apply color to a row
+	ColorRowCode [2]string
+	// Function which will eval whether to apply color to a col
+	// ColorColCond    func(t *Table) (bool, error)
+	// ColorColCode [2]string
 }
 
-func (t *Table) PrintAll() error {
+type Row struct {
+	Raw      []string
+	Rendered string
+	Colored  bool
+}
+
+func (t *Table) AddRow(row []string, color bool) {
+	if len(t.RenderedPadding) == 0 {
+		p := t.GetPadding()
+		t.RenderedPadding = p
+	}
+
+	ri := strSliceToInterSlice(row)
+
+	rr := fmt.Sprintf(t.RenderedPadding, ri...)
+
+	r := Row{
+		Raw:      row,
+		Rendered: rr,
+		Colored:  color,
+	}
+
+	t.Rows = append(t.Rows, r)
+}
+
+func (t *Table) Print() error {
 	if len(t.Rows) == 0 && len(t.NoResultsMsg) != 0 {
 		fmt.Println(t.NoResultsMsg)
 		return nil
@@ -30,33 +78,28 @@ func (t *Table) PrintAll() error {
 }
 
 func (t *Table) PrintHeader() {
-	header := strSliceToInterSlice(t.Header)
-	h := fmt.Sprintf(t.BuildPadding(), header...)
+	if len(t.RenderedPadding) == 0 {
+		p := t.GetPadding()
+		t.RenderedPadding = p
+	}
 
-	fmt.Println(h)
+	header := strSliceToInterSlice(t.Header)
+	t.RenderedHeader = fmt.Sprintf(t.RenderedPadding, header...)
+
+	fmt.Println(t.RenderedHeader)
 }
 
 func (t *Table) PrintRows() {
-	t.BuildRows()
-	for _, r := range t.RenderedRows {
-		fmt.Println(r)
-	}
-}
-
-func (t *Table) BuildRows() {
-	// TODO This function could trim strings down to length of padding
-	// at the index of the column
-	p := t.BuildPadding()
-	var rows []string
 	for _, r := range t.Rows {
-		row := strSliceToInterSlice(r)
-		rows = append(rows, fmt.Sprintf(p, row...))
+		if r.Colored && len(t.ColorRowCode) == 2 {
+			fmt.Println(t.ColorRowCode[0] + r.Rendered + t.ColorRowCode[1])
+		} else {
+			fmt.Println(r.Rendered)
+		}
 	}
-
-	t.RenderedRows = rows
 }
 
-func (t *Table) BuildPadding() string {
+func (t *Table) GetPadding() string {
 	padStr := " "
 	for _, x := range t.Padding {
 		temp := "%ds"
