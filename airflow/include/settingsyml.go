101490c7c169433cc6db63f42c4762bcf13088bf
Benjamin Gregory <benjaminrgregory@gmail.com>
2019-01-11 14:02:27 -0700

    Add additional note for conn_extra formatting

--- airflow/include/settingsyml.go
+++ airflow/include/settingsyml.go
@@ -4,20 +4,25 @@ import "strings"
 
 // Settingsyml is the settings template
 var Settingsyml = strings.TrimSpace(`
-  # This feature is in Beta.
-  # Please report any bugs to https://github.com/astronomer/astro-cli/issues
-  airflow:
-    connections:
-      - conn_id:
-        conn_type:
-        conn_host:
-        conn_login:
-        conn_password:
-        conn_port:
-    pools:
-      - pool_name:
-        pool_slot:
-        pool_description:
-    variables:
-      - variable_name:
-        variable_value:`)
+# This feature is in Beta.
+
+# Please report any bugs to https://github.com/astronomer/astro-cli/issues
+
+# NOTE: If putting a dict in conn_extra, please wrap in single quotes.
+
+airflow:
+  connections:
+    - conn_id:
+      conn_type:
+      conn_host:
+      conn_login:
+      conn_password:
+      conn_port:
+      conn_extra:
+  pools:
+    - pool_name:
+      pool_slot:
+      pool_description:
+  variables:
+    - variable_name:
+      variable_value:`)
