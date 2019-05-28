e61baad679309be419a9752091d371331d3030cb
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-05-28 14:39:25 +0300

    Add create_user during astro airflow start

--- airflow/docker.go
+++ airflow/docker.go
@@ -254,6 +254,12 @@ func Start(airflowHome string, envFile string) error {
 	parts := strings.Split(config.CFG.WebserverPort.GetString(), ":")
 	fmt.Printf(messages.COMPOSE_LINK_WEBSERVER+"\n", parts[len(parts)-1])
 	fmt.Printf(messages.COMPOSE_LINK_POSTGRES+"\n", config.CFG.PostgresPort.GetString())
+
+	// Run create_user in separate goroutine to never block airflow start
+	func() {
+		_ = Run(airflowHome, []string{"airflow", "create_user", "-r", "Admin", "-u", "admin", "-e", "admin@example.com", "-f", "admin", "-l", "user", "-p", "admin"})
+	}()
+
 	return nil
 }
 
