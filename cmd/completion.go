27fcd29bb71af93b97f1def2eef19b85ac04ba74
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-09-03 21:23:48 +0300

    split bash/zsh

--- cmd/completion.go
+++ cmd/completion.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -29,27 +31,6 @@ var (
 	}
 )
 
-
-func newCompletionCmd(out io.Writer) *cobra.Command {
-	var shells []string
-	for s := range completionShells {
-		shells = append(shells, s)
-	}
-
-	cmd := &cobra.Command{
-		Use:   "completion SHELL",
-		Short: "Generate autocompletions script for the specified shell (bash or zsh)",
-		Long:  completionDesc,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return runCompletion(out, cmd, args)
-		},
-		ValidArgs: shells,
-	}
-
-	return cmd
-}
-
-
 func runCompletion(out io.Writer, cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
 		return errors.New("shell not specified")
@@ -65,25 +46,45 @@ func runCompletion(out io.Writer, cmd *cobra.Command, args []string) error {
 	return run(out, cmd)
 }
 
-func runCompletionBash(out io.Writer, cmd *cobra.Command) error {
-	return cmd.Root().GenBashCompletion(out)
+func runCompletionBash(_ io.Writer, cmd *cobra.Command) error {
+	var buf bytes.Buffer
+	err := cmd.Root().GenBashCompletion(&buf)
+	if err != nil {
+		return fmt.Errorf("error while generating bash completion: %v", err)
+	}
+	// remove the command "completion" from auto-completion
+	code := buf.String()
+	code = strings.Replace(code, `commands+=("completion")`, "", -1)
+
+	fmt.Print(code)
+	return nil
 }
 
-func runCompletionZsh(out io.Writer, cmd *cobra.Command) error {
-	zshInitialization := `#compdef astro
+func runCompletionZsh(_ io.Writer, cmd *cobra.Command) error {
+	var buf bytes.Buffer
 
+	// zshHead is the header required to declare zsh completion
+	zshHead := "#compdef astro\n"
+
+	// zshInit represents initialization code needed to convert bash completion
+	// code to zsh completion.
+
+	zshInitialization := `
 __astro_bash_source() {
 	alias shopt=':'
 	alias _expand=_bash_expand
 	alias _complete=_bash_comp
 	emulate -L sh
 	setopt kshglob noshglob braceexpand
+
 	source "$@"
 }
+
 __astro_type() {
 	# -t is not supported by zsh
 	if [ "$1" == "-t" ]; then
 		shift
+
 		# fake Bash 4 to disable "complete -o nospace". Instead
 		# "compopt +-o nospace" is used in the code to toggle trailing
 		# spaces. We don't support that, but leave trailing spaces on
@@ -95,9 +96,11 @@ __astro_type() {
 	fi
 	type "$@"
 }
+
 __astro_compgen() {
 	local completions w
 	completions=( $(compgen "$@") ) || return $?
+
 	# filter by given word as prefix
 	while [[ "$1" = -* && "$1" != -- ]]; do
 		shift
@@ -115,13 +118,7 @@ __astro_compgen() {
 __astro_compopt() {
 	true # don't do anything. Not supported by bashcompinit in zsh
 }
-__astro_declare() {
-	if [ "$1" == "-F" ]; then
-		whence -w "$@"
-	else
-		builtin declare "$@"
-	fi
-}
+
 __astro_ltrim_colon_completions()
 {
 	if [[ "$1" == *:* && "$COMP_WORDBREAKS" == *:* ]]; then
@@ -133,20 +130,24 @@ __astro_ltrim_colon_completions()
 		done
 	fi
 }
+
 __astro_get_comp_words_by_ref() {
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[${COMP_CWORD}-1]}"
 	words=("${COMP_WORDS[@]}")
 	cword=("${COMP_CWORD[@]}")
 }
+
 __astro_filedir() {
 	local RET OLD_IFS w qw
+
 	__debug "_filedir $@ cur=$cur"
 	if [[ "$1" = \~* ]]; then
 		# somehow does not work. Maybe, zsh does not call this at all
 		eval echo "$1"
 		return 0
 	fi
+
 	OLD_IFS="$IFS"
 	IFS=$'\n'
 	if [ "$1" = "-d" ]; then
@@ -156,7 +157,9 @@ __astro_filedir() {
 		RET=( $(compgen -f) )
 	fi
 	IFS="$OLD_IFS"
+
 	IFS="," __debug "RET=${RET[@]} len=${#RET[@]}"
+
 	for w in ${RET[@]}; do
 		if [[ ! "${w}" = "${cur}"* ]]; then
 			continue
@@ -171,22 +174,26 @@ __astro_filedir() {
 		fi
 	done
 }
+
 __astro_quote() {
-	if [[ $1 == \'* || $1 == \"* ]]; then
-		# Leave out first character
-		printf %q "${1:1}"
-	else
-		printf %q "$1"
-	fi
+    if [[ $1 == \'* || $1 == \"* ]]; then
+        # Leave out first character
+        printf %q "${1:1}"
+    else
+    	printf %q "$1"
+    fi
 }
+
 autoload -U +X bashcompinit && bashcompinit
+
 # use word boundary patterns for BSD or GNU sed
 LWORD='[[:<:]]'
 RWORD='[[:>:]]'
-if sed --help 2>&1 | grep -q 'GNU\|BusyBox'; then
+if sed --help 2>&1 | grep -q GNU; then
 	LWORD='\<'
 	RWORD='\>'
 fi
+
 __astro_convert_bash_to_zsh() {
 	sed \
 	-e 's/declare -F/whence -w/' \
@@ -199,23 +206,44 @@ __astro_convert_bash_to_zsh() {
 	-e "s/${LWORD}__ltrim_colon_completions${RWORD}/__astro_ltrim_colon_completions/g" \
 	-e "s/${LWORD}compgen${RWORD}/__astro_compgen/g" \
 	-e "s/${LWORD}compopt${RWORD}/__astro_compopt/g" \
-	-e "s/${LWORD}declare${RWORD}/__astro_declare/g" \
-	-e "s/\\\$(type${RWORD}/\$(__astro_type/g" \
+	-e "s/${LWORD}declare${RWORD}/builtin declare/g" \
 	-e 's/aliashash\["\(.\{1,\}\)"\]/aliashash[\1]/g' \
+	-e "s/\\\$(type${RWORD}/\$(__astro_type/g" \
 	<<'BASH_COMPLETION_EOF'
 `
-	out.Write([]byte(zshInitialization))
 
-	buf := new(bytes.Buffer)
-	cmd.Root().GenBashCompletion(buf)
-	out.Write(buf.Bytes())
-
-	zshTail := `
+	// zshFinalize is code going to the end of completion file
+	// that calls conversion bash to zsh.
+	zshFinalize := `
 BASH_COMPLETION_EOF
 }
+
 __astro_bash_source <(__astro_convert_bash_to_zsh)
 `
-	out.Write([]byte(zshTail))
+	_, err := buf.Write([]byte(zshHead))
+	if err != nil {
+		return fmt.Errorf("error while generating zsh completion: %v", err)
+	}
+
+	_, err = buf.Write([]byte(zshInitialization))
+	if err != nil {
+		return fmt.Errorf("error while generating zsh completion: %v", err)
+	}
+	err = cmd.GenBashCompletion(&buf)
+	if err != nil {
+		return fmt.Errorf("error wheil generating zsh completion: %v", err)
+	}
+
+	_, err = buf.Write([]byte(zshFinalize))
+	if err != nil {
+		return fmt.Errorf("error while generating zsh completion: %v", err)
+	}
+
+	// remove the command "completion" from auto-completion
+	code := buf.String()
+	code = strings.Replace(code, `commands+=("completion")`, "", -1)
+
+	fmt.Print(code)
 	return nil
 }
 
