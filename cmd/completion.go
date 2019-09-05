84e008da916ea32e3c988d528936ba1de20e6b9d
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-09-05 16:41:07 +0300

    Add ability to generate bash completion to astro cli (#277)
    * still WIP
    
    * add bash/zsh
    
    * split bash/zsh
    
    * add more docs
    
    * simlify error catching
    
    * switch cmd.Root().GenZshCompletion(&buf)
    
    * polish code a little bit
    
    * fix completion long example
    
    * make completion hidden from tab completion

---
+++ cmd/completion.go
@@ -0,0 +1,89 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+const completionLong = `
+Generate autocompletions script for Astro for the specified shell (bash or zsh).
+
+This command can generate shell autocompletions. e.g.
+
+	$ astro completion bash
+
+Can be sourced as such
+
+	$ source <(astro completion bash)
+
+Bash users can as well save it to the file and copy it to:
+	/etc/bash_completion.d/
+
+Correct arguments for SHELL are: "bash" and "zsh".
+Notes:
+
+1) zsh completions requires zsh 5.2 or newer.
+	
+2) macOS users have to install bash-completion framework to utilize
+completion features. This can be done using homebrew:
+	brew install bash-completion
+
+Once installed, you must load bash_completion by adding following
+line to your .profile or .bashrc/.zshrc:
+	source $(brew --prefix)/etc/bash_completion
+
+3) For oh-my-zsh users make sure you have added to your ~/.zshrc
+	autoload -Uz compinit && compinit -C
+`
+
+var (
+	completionShells = map[string]func(out io.Writer, cmd *cobra.Command) error{
+		"bash": runCompletionBash,
+		"zsh":  runCompletionZsh,
+	}
+)
+
+func runCompletion(out io.Writer, cmd *cobra.Command, args []string) error {
+	cmd.Hidden = true
+	return completionShells[args[0]](out, cmd)
+}
+
+func runCompletionBash(w io.Writer, cmd *cobra.Command) error {
+	err := cmd.Root().GenBashCompletion(w)
+	if err != nil {
+		return fmt.Errorf("error while generating bash completion: %v", err)
+	}
+	return nil
+}
+
+func runCompletionZsh(w io.Writer, cmd *cobra.Command) error {
+	err := cmd.Root().GenZshCompletion(w)
+	if err != nil {
+		return fmt.Errorf("error while generating bash completion: %v", err)
+	}
+	return nil
+}
+
+func init() {
+	var shells []string
+	for s := range completionShells {
+		shells = append(shells, s)
+	}
+
+	cmd := &cobra.Command{
+		Use:   "completion SHELL",
+		Short: "Generate autocompletions script for the specified shell (bash or zsh)",
+		Long:  completionLong,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runCompletion(os.Stdout, cmd, args)
+		},
+		Args: cobra.ExactValidArgs(1),
+		ValidArgs: shells,
+	}
+
+	RootCmd.AddCommand(cmd)
+}
