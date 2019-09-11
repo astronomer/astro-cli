5a4aa3be7affcbbba34e2e36c9bab39e46e4690d
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-09-11 17:01:53 +0300

    Fix error message when deployment name (#290)
    * fix error message
    
    * add simple test

---
+++ airflow/docker_test.go
@@ -0,0 +1,28 @@
+package airflow
+
+import (
+	"testing"
+
+	"github.com/astronomer/astro-cli/houston"
+)
+
+func TestDeploymentNameExists(t *testing.T) {
+	deployments := []houston.Deployment{
+		{ReleaseName: "dev"},
+		{ReleaseName: "dev1"},
+	}
+	exists := deploymentNameExists("dev", deployments)
+	if !exists {
+		t.Errorf("deploymentNameExists(dev) = %t; want true", exists)
+	}
+}
+
+func TestDeploymentNameDoesntExists(t *testing.T) {
+	deployments := []houston.Deployment{
+		{ReleaseName: "dummy"},
+	}
+	exists := deploymentNameExists("dev", deployments)
+	if exists {
+		t.Errorf("deploymentNameExists(dev) = %t; want false", exists)
+	}
+}
