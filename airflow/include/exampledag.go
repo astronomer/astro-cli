932f91ca1c3e6a4134473f552718d797e632658f
Andrew Harmon <andrew@astronomer.io>
2019-12-12 13:17:27 -0500

    update example dag file (#315)
    I've written an entirely new example dag that to used when astro init is run

--- airflow/include/exampledag.go
+++ airflow/include/exampledag.go
@@ -5,94 +5,67 @@ import "strings"
 // Exampledag created with astro airflow init
 var Exampledag = strings.TrimSpace(`
 from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
+
+def my_custom_function(ts,**kwargs):
+    """
+    This can be any python code you want and is called from the python operator. The code is not executed until
+    the task is run by the airflow scheduler.
+    """
+    print(f"I am task number {kwargs['task_number']}. This DAG Run execution date is {ts} and the current time is {datetime.now()}")
+    print('Here is the full DAG Run context. It is available because provide_context=True')
+    print(kwargs)
+
+
+# Default settings applied to all tasks
 default_args = {
-	'owner': 'airflow',
-	'depends_on_past': False,
-	'start_date': datetime(2018, 1, 1),
-	'email_on_failure': False,
-	'email_on_retry': False,
-	'retries': 1,
-	'retry_delay': timedelta(minutes=5),
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG('example_dag',
-			max_active_runs=3,
-			schedule_interval=timedelta(minutes=5),
-			default_args=default_args)
+# Using a DAG context manager, you don't have to specify the dag property of each task
+with DAG('example_dag',
+         start_date=datetime(2019, 1, 1),
+         max_active_runs=3,
+         schedule_interval=timedelta(minutes=30),  # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
+         default_args=default_args,
+         # catchup=False # enable if you don't want historical dag runs to run
+         ) as dag:
 
-t1 = BashOperator(
-	task_id='print_date1',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
+    t0 = DummyOperator(
+        task_id='start'
+    )
 
-t2 = BashOperator(
-	task_id='print_date2',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
+    t1 = DummyOperator(
+        task_id='group_bash_tasks'
+    )
+    t2 = BashOperator(
+        task_id='bash_print_date1',
+        bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s && date')
+    t3 = BashOperator(
+        task_id='bash_print_date2',
+        bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s && date')
 
-t3 = BashOperator(
-	task_id='print_date3',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
+    # generate tasks with a loop. task_id must be unique
+    for task in range(5):
+        tn = PythonOperator(
+            task_id=f'python_print_date_{task}',
+            python_callable=my_custom_function,  # make sure you don't include the () of the function
+            op_kwargs={'task_number': task},
+            provide_context=True
+        )
 
-t4 = BashOperator(
-	task_id='print_date4',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
 
-t5 = BashOperator(
-	task_id='print_date5',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
+        t0 >> tn # indented inside for loop so each task is added downstream of t0
 
-t6 = BashOperator(
-	task_id='print_date6',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t7 = BashOperator(
-	task_id='print_date7',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t8 = BashOperator(
-	task_id='print_date8',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t2.set_upstream(t1)
-t3.set_upstream(t2)
-t4.set_upstream(t3)
-t5.set_upstream(t3)
-t6.set_upstream(t3)
-t7.set_upstream(t3)
-t8.set_upstream(t3)
-
-t9 = BashOperator(
-	task_id='print_date9',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t10 = BashOperator(
-	task_id='print_date10',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t11 = BashOperator(
-	task_id='print_date11',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t12 = BashOperator(
-	task_id='print_date12',
-	bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s',
-	dag=dag)
-
-t9.set_upstream(t8)
-t10.set_upstream(t8)
-t11.set_upstream(t8)
-t12.set_upstream(t8)
+    t0 >> t1
+    t1 >> [t2, t3] # lists can be used to specify mutliple tasks
 `)
