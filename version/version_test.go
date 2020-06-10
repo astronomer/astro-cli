55f4fb5d25dfa5c1f9bf1800129869cd8eb769e1
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-06-10 20:02:41 +0300

    add more tests

--- version/version_test.go
+++ version/version_test.go
@@ -1,8 +1,16 @@
 package version
 
 import (
-	"github.com/stretchr/testify/assert"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/astronomer/astro-cli/houston"
+	"github.com/astronomer/astro-cli/messages"
+	testUtil "github.com/astronomer/astro-cli/pkg/testing"
 )
 
 func TestIsValidVersionInValid(t *testing.T) {
@@ -11,4 +19,31 @@ func TestIsValidVersionInValid(t *testing.T) {
 
 func TestIsValidVersionValid(t *testing.T) {
 	assert.True(t, isValidVersion("0.0.1"))
+}
+
+func TestPrintVersionError(t *testing.T) {
+	testUtil.InitTestConfig()
+	okResponse := `{
+ "data": {
+   "appConfig": {
+     "version": "0.19.1",
+     "baseDomain": "local.astronomer.io",
+     "smtpConfigured": true,
+     "manualReleaseNames": false
+   }
+ }
+}`
+	client := testUtil.NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(okResponse)),
+			Header:     make(http.Header),
+		}
+	})
+	api := houston.NewHoustonClient(client)
+	output := new(bytes.Buffer)
+	err := PrintVersion(api, output)
+	if assert.Error(t, err, "An error was expected") {
+		assert.EqualError(t, err, messages.ERROR_INVALID_CLI_VERSION)
+	}
 }
