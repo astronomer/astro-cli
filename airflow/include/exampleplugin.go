9e18ae4005e48e98c9f0d14dd7702c89c583e61e
Kaxil Naik <kaxilnaik@gmail.com>
2020-10-13 22:13:50 +0100

    Make Example Plugin 2.0 compatible

--- airflow/include/exampleplugin.go
+++ airflow/include/exampleplugin.go
@@ -5,42 +5,65 @@ import "strings"
 // ExamplePlugin created with astro airflow init
 var ExamplePlugin = strings.TrimSpace(`
 from airflow.plugins_manager import AirflowPlugin
-from flask_admin.base import MenuLink
+from airflow.version import version
 
 """
 Look for the Astronomer tab in the UI.
 """
-airflow_plugins_ml = MenuLink(
-    category='Astronomer',
-    name='Airflow-Plugins',
-    url='https://github.com/airflow-plugins/')
+airflow_plugins_ml = {
+    "name": "Airflow-Plugins",
+    "category": "Astronomer",
+    "category_icon": "fa-rocket",
+    "href": "https://github.com/airflow-plugins/"
+}
 
-astro_docs_ml = MenuLink(
-    category='Astronomer',
-    name='Astronomer Docs',
-    url='https://www.astronomer.io/docs/')
+astro_docs_ml = {
+    "name": "Astronomer Docs",
+    "category": "Astronomer",
+    "category_icon": "fa-rocket",
+    "href": "https://www.astronomer.io/docs/"
+}
 
-astro_guides_ml = MenuLink(
-    category='Astronomer',
-    name='Airflow Guides',
-    url='https://www.astronomer.io/guides/')
+astro_guides_ml = {
+    "name": "Airflow Guide",
+    "category": "Astronomer",
+    "category_icon": "fa-rocket",
+    "href": "https://www.astronomer.io/guides/"
+}
 
+_appbuilder_menu_items = [airflow_plugins_ml, astro_docs_ml, astro_guides_ml]
 
-class AstroLinksPlugin(AirflowPlugin):
-    name = 'astronomer_menu_links'
-    operators = []
-    flask_blueprints = []
-    hooks = []
-    executors = []
-    macros = []
-    admin_views = []
-    menu_links = [airflow_plugins_ml, astro_docs_ml, astro_guides_ml]
-    appbuilder_views = []
-    appbuilder_menu_items = [
-        {
-            "name": ml.name,
-            "category": ml.category,
-            "category_icon": "fa-rocket",
-            "href": ml.url,
-        } for ml in menu_links
-    ]`)
+# Airflow >= 2.0
+if version.startswith("2"):
+    class AstroLinksPlugin(AirflowPlugin):
+        name = 'astronomer_menu_links'
+        operators = []
+        flask_blueprints = []
+        hooks = []
+        executors = []
+        macros = []
+        admin_views = []
+        appbuilder_views = []
+        appbuilder_menu_items = [airflow_plugins_ml, astro_docs_ml, astro_guides_ml]
+else:
+    # Airflow < 2.0
+    from flask_admin.base import MenuLink
+
+    class AstroLinksPlugin(AirflowPlugin):
+        name = 'astronomer_menu_links'
+        operators = []
+        flask_blueprints = []
+        hooks = []
+        executors = []
+        macros = []
+        admin_views = []
+        menu_links = [
+            MenuLink(
+                category=ml.get("category"),
+                name=ml.get("name"),
+                url=ml.get("href")
+            ) for ml in _appbuilder_menu_items
+        ]
+        appbuilder_views = []
+        appbuilder_menu_items = _appbuilder_menu_items
+`)
