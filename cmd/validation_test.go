36f5c678a2a54f12d607b64af8b669c78a8490f3
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2021-04-26 22:46:32 +0300

    add tests

--- cmd/validation_test.go
+++ cmd/validation_test.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidateInvalidRole(t *testing.T) {
@@ -15,5 +17,37 @@ func TestValidateValidRole(t *testing.T) {
 	err := validateRole("admin")
 	if err != nil {
 		t.Errorf("%s", err)
+	}
+}
+
+func TestValidateDagDeploymentArgs(t *testing.T) {
+	myTests := []struct {
+		dagDeploymentType, nfsLocation string
+		expectedOutput                 string
+		expectedError                  error
+	}{
+		{dagDeploymentType: "volume", nfsLocation: "test:/test", expectedError: nil},
+		{dagDeploymentType: "image", expectedError: nil},
+	}
+
+	for _, tt := range myTests {
+		actualError := validateDagDeploymentArgs(tt.dagDeploymentType, tt.nfsLocation)
+		assert.NoError(t, actualError, "optional message here")
+	}
+}
+
+func TestValidateDagDeploymentArgsErrors(t *testing.T) {
+	myTests := []struct {
+		dagDeploymentType, nfsLocation string
+		expectedOutput                 string
+		expectedError                  string
+	}{
+		{dagDeploymentType: "volume", expectedError: "please specify nfs location via --nfs-location flag"},
+		{dagDeploymentType: "unknown", expectedError: "please specify correct dag deployment type, one of: image, volume"},
+	}
+
+	for _, tt := range myTests {
+		actualError := validateDagDeploymentArgs(tt.dagDeploymentType, tt.nfsLocation)
+		assert.EqualError(t, actualError, tt.expectedError, "optional message here")
 	}
 }
