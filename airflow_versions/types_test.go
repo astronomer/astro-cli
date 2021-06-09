cec0d3097e4a2e13c37afcbf214d7758e3a40ca0
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2021-06-09 21:33:45 +0300

    add more tests

--- airflow_versions/types_test.go
+++ airflow_versions/types_test.go
@@ -15,6 +15,12 @@ func TestNewAirflowVersion(t *testing.T) {
 	assert.Equal(t, av.postN1, uint64(1))
 }
 
+func TestNewAirflowVersionError(t *testing.T) {
+	av, err := NewAirflowVersion("-1", []string{"2.0.0-1-buster-onbuild"})
+	assert.Error(t, err)
+	assert.Nil(t, av)
+}
+
 func TestNewAirflowVersionWithoutPostN1(t *testing.T) {
 	av, err := NewAirflowVersion("2.0.0", []string{"2.0.0-buster-onbuild"})
 	assert.NoError(t, err)
@@ -39,4 +45,42 @@ func TestCompareAirflowVersions(t *testing.T) {
 	av2, err := NewAirflowVersion("1.10.5", []string{"1.10.5-alpine3.10-onbuild"})
 	assert.NoError(t, err)
 	assert.Equal(t, av1.Compare(av2), 1)
+}
+
+func TestCompareAirflowVersionsMajor(t *testing.T) {
+	av1, err := NewAirflowVersion("2.10.5-11", []string{"2.10.5-11-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	av2, err := NewAirflowVersion("1.10.5", []string{"1.10.5-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	assert.Equal(t, av1.Compare(av2), 1)
+}
+
+func TestCompareAirflowVersionsMinor(t *testing.T) {
+	av1, err := NewAirflowVersion("1.11.5-11", []string{"1.11.5-11-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	av2, err := NewAirflowVersion("1.10.5", []string{"1.10.5-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	assert.Equal(t, av1.Compare(av2), 1)
+}
+
+func TestCompareAirflowVersionsPatch(t *testing.T) {
+	av1, err := NewAirflowVersion("1.10.6", []string{"1.11.6-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	av2, err := NewAirflowVersion("1.10.5", []string{"1.10.5-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	assert.Equal(t, av1.Compare(av2), 1)
+}
+
+func TestGreaterThan(t *testing.T) {
+	av1, err := NewAirflowVersion("1.10.5-11", []string{"1.10.5-11-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	av2, err := NewAirflowVersion("1.10.5", []string{"1.10.5-alpine3.10-onbuild"})
+	assert.NoError(t, err)
+	assert.Equal(t, av1.GreaterThan(av2), true)
+}
+
+func Test_compareSegment(t *testing.T) {
+	assert.Equal(t, compareSegment(1, 1), 0)
+	assert.Equal(t, compareSegment(1, 2), -1)
+	assert.Equal(t, compareSegment(2, 1), 1)
 }
