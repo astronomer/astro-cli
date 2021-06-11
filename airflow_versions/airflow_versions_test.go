71a51d5e07ce6bbb3e719fbdcfad6a35ec788cc8
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2021-06-11 19:18:48 +0300

    tweak tests

--- airflow_versions/airflow_versions_test.go
+++ airflow_versions/airflow_versions_test.go
@@ -16,6 +16,19 @@ func TestGetDefaultImageTag(t *testing.T) {
   "version": "1.0",
   "available_releases": [
     {
+      "version": "1.10.4-11",
+      "level": "bug_fix",
+      "url": "https://github.com/astronomer/airflow/releases/tag/1.10.4-11",
+      "release_date": "2020-9-05T20:03:00+00:00",
+      "tags": [
+        "1.10.4-11-alpine3.10-onbuild",
+        "1.10.4-11-buster-onbuild",
+        "1.10.4-11-alpine3.10",
+        "1.10.4-11-buster"
+      ],
+      "channel": "stable"
+    },
+    {
       "version": "1.10.5",
       "level": "new_feature",
       "url": "https://github.com/astronomer/airflow/releases/tag/1.10.5-11",
@@ -54,7 +67,7 @@ func TestGetDefaultImageTag(t *testing.T) {
 
 	defaultImageTag, err := GetDefaultImageTag(httpClient, "")
 	assert.NoError(t, err)
-	assert.Equal(t, defaultImageTag, "1.10.5-buster-onbuild")
+	assert.Equal(t, "1.10.5-buster-onbuild", defaultImageTag)
 }
 
 func TestGetDefaultImageTagError(t *testing.T) {
@@ -71,5 +84,5 @@ func TestGetDefaultImageTagError(t *testing.T) {
 
 	defaultImageTag, err := GetDefaultImageTag(httpClient, "")
 	assert.Error(t, err)
-	assert.Equal(t, defaultImageTag, "")
+	assert.Equal(t, "", defaultImageTag)
 }
