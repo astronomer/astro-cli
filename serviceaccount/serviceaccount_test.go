2df8a5b32c62e82702e22cedb9a66fef01ba7d67
bote795 <nbotell2@gmail.com>
2021-07-09 09:49:14 -0500

    add tests for service account

--- serviceaccount/serviceaccount_test.go
+++ serviceaccount/serviceaccount_test.go
@@ -2,12 +2,13 @@ package deployment
 
 import (
 	"bytes"
-	"github.com/astronomer/astro-cli/houston"
-	testUtil "github.com/astronomer/astro-cli/pkg/testing"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
 	"testing"
+
+	"github.com/astronomer/astro-cli/houston"
+	testUtil "github.com/astronomer/astro-cli/pkg/testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateUsingDeploymentUUID(t *testing.T) {
@@ -146,4 +147,78 @@ func TestDeleteUsingDeploymentUUID(t *testing.T) {
 	expectedOut := `Service Account  (ckbvcbqs1014t0760u4bszmcs) successfully deleted
 `
 	assert.Equal(t, buf.String(), expectedOut)
+}
+
+func TestGetDeploymentServiceAccount(t *testing.T) {
+	testUtil.InitTestConfig()
+	okResponse := `
+	{
+		"data": {
+		  "deploymentServiceAccounts": [
+		    {
+		      "id": "ckqvfa2cu1468rn9hnr0bqqfk",
+		      "apiKey": "658b304f36eaaf19860a6d9eb73f7d8a",
+		      "label": "yooo can u see me test",
+		      "category": "",
+		      "entityType": "DEPLOYMENT",
+		      "entityUuid": null,
+		      "active": true,
+		      "createdAt": "2021-07-08T21:28:57.966Z",
+		      "updatedAt": "2021-07-08T21:28:57.967Z",
+		      "lastUsedAt": null
+		    }
+		  ]
+		}
+	      }`
+	client := testUtil.NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(okResponse)),
+			Header:     make(http.Header),
+		}
+	})
+	api := houston.NewHoustonClient(client)
+	deploymentUuid := "ckqvf9spa1189rn9hbh5h439u"
+	buf := new(bytes.Buffer)
+	err := GetDeploymentServiceAccounts(deploymentUuid, api, buf)
+	assert.NoError(t, err)
+	expectedOut := ` yooo can u see me test                  ckqvfa2cu1468rn9hnr0bqqfk     658b304f36eaaf19860a6d9eb73f7d8a`
+	assert.Contains(t, buf.String(), expectedOut)
+}
+
+func TestGetWorkspaceServiceAccount(t *testing.T) {
+	testUtil.InitTestConfig()
+	okResponse := `
+	{
+		"data": {
+		  "workspaceServiceAccounts": [
+		    {
+		      "id": "ckqvfa2cu1468rn9hnr0bqqfk",
+		      "apiKey": "658b304f36eaaf19860a6d9eb73f7d8a",
+		      "label": "yooo can u see me test",
+		      "category": "",
+		      "entityType": "DEPLOYMENT",
+		      "entityUuid": null,
+		      "active": true,
+		      "createdAt": "2021-07-08T21:28:57.966Z",
+		      "updatedAt": "2021-07-08T21:28:57.967Z",
+		      "lastUsedAt": null
+		    }
+		  ]
+		}
+	      }`
+	client := testUtil.NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(okResponse)),
+			Header:     make(http.Header),
+		}
+	})
+	api := houston.NewHoustonClient(client)
+	deploymentUuid := "ckqvf9spa1189rn9hbh5h439u"
+	buf := new(bytes.Buffer)
+	err := GetWorkspaceServiceAccounts(deploymentUuid, api, buf)
+	assert.NoError(t, err)
+	expectedOut := ` yooo can u see me test                  ckqvfa2cu1468rn9hnr0bqqfk     658b304f36eaaf19860a6d9eb73f7d8a`
+	assert.Contains(t, buf.String(), expectedOut)
 }
