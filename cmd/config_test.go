27263e6664307af724870282ccab28ae99617b4d
Neel Dalsania <neel.dalsania@astronomer.io>
2021-10-21 17:23:27 +0530

    added new test cases

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -13,3 +13,28 @@ func TestConfigRootCommand(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Contains(t, output, "astro config")
 }
+
+func TestConfigGetCommandSuccess(t *testing.T) {
+	testUtil.InitTestConfig()
+	_, err := executeCommand("config", "get", "project.name", "-g")
+	assert.NoError(t, err)
+}
+
+func TestConfigGetCommandFailure(t *testing.T) {
+	testUtil.InitTestConfig()
+	output, err := executeCommand("config", "get", "-g", "test")
+	assert.Error(t, err)
+	assert.Contains(t, output, "Config does not exist, check your config key")
+}
+
+func TestConfigSetCommandFailure(t *testing.T) {
+	testUtil.InitTestConfig()
+	_, err := executeCommand("config", "set", "test", "testing", "-g")
+	assert.Error(t, err)
+}
+
+func TestConfigSetCommandFailurePrjConfig(t *testing.T) {
+	testUtil.InitTestConfig()
+	_, err := executeCommand("config", "set", "-g", "project.name", "testing")
+	assert.Error(t, err)
+}
--- config/types_test.go
+++ config/types_test.go
@@ -3,10 +3,80 @@ package config
 import (
 	"testing"
 
+	"github.com/spf13/afero"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCfg(t *testing.T) {
-	cfg := newCfg("foo", "bar")
-	assert.NotNil(t, cfg)
+	c := newCfg("foo", "bar")
+	assert.NotNil(t, c)
+}
+
+func Test_cfg_GetString(t *testing.T) {
+	type fields struct {
+		Path    string
+		Default string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "basic test case",
+			fields: fields{
+				Path:    "foo",
+				Default: "bar",
+			},
+			want: "bar",
+		},
+	}
+	for _, tt := range tests {
+		fs := afero.NewOsFs()
+		InitConfig(fs)
+		t.Run(tt.name, func(t *testing.T) {
+			c := cfg{
+				Path:    tt.fields.Path,
+				Default: tt.fields.Default,
+			}
+			if got := c.GetString(); got != tt.want {
+				t.Errorf("cfg.GetString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cfg_GetBool(t *testing.T) {
+	type fields struct {
+		Path    string
+		Default string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "basic test case",
+			fields: fields{
+				Path:    "foo",
+				Default: "bar",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		fs := afero.NewOsFs()
+		InitConfig(fs)
+		t.Run(tt.name, func(t *testing.T) {
+			c := cfg{
+				Path:    tt.fields.Path,
+				Default: tt.fields.Default,
+			}
+			if got := c.GetBool(); got != tt.want {
+				t.Errorf("cfg.GetString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
