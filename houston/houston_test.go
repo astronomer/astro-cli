3dfb118660e332ccb73996558e6b6358aae7c703
Rujhan Arora <95576577+rujhan-arora-astronomer@users.noreply.github.com>
2022-03-30 12:50:15 +0530

    If Houston-API throws ErrAuthTokenRefreshFailed error, then show ErrVerboseInaptPermissions message in the cli (#519)
    * Added verbose error for Auth Token Refresh Failed Houston Error
    
    * added TestErrAuthTokenRefreshFailed
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added TestErrAuthTokenRefreshFailed
    
    * added TestErrAuthTokenRefreshFailed
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- houston/houston_test.go
+++ houston/houston_test.go
@@ -1,9 +1,14 @@
 package houston
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/astronomer/astro-cli/pkg/httputil"
+	testUtil "github.com/astronomer/astro-cli/pkg/testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,4 +16,38 @@ import (
 func TestNewHoustonClient(t *testing.T) {
 	client := newInternalClient(httputil.NewHTTPClient())
 	assert.NotNil(t, client, "Can't create new houston Client")
+}
+
+func TestErrAuthTokenRefreshFailed(t *testing.T) {
+	testUtil.InitTestConfig()
+	mockResponse := &Response{
+		Data: ResponseData{},
+		Errors: []Error{
+			{
+				Message: ErrAuthTokenRefreshFailed.Error(),
+				Name:    "",
+			},
+		},
+	}
+	jsonResponse, _ := json.Marshal(mockResponse)
+
+	t.Run("Test ErrAuthTokenRefreshFailed error", func(t *testing.T) {
+		client := testUtil.NewTestClient(func(req *http.Request) *http.Response {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewBuffer(jsonResponse)),
+				Header:     make(http.Header),
+			}
+		})
+		doOpts := httputil.DoOptions{
+			Headers: map[string]string{
+				"Accept": "application/json",
+			},
+		}
+		houstonClient := &Client{HTTPClient: client}
+		resp, err := houstonClient.Do(doOpts)
+
+		assert.Contains(t, err.Error(), ErrVerboseInaptPermissions.Error())
+		assert.Nil(t, resp)
+	})
 }
