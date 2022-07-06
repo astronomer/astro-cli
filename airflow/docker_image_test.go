4dd3f6f6232248ebef9cb20cb69e1b16e800d765
sunkickr <davidkoenit@gmail.com>
2022-07-06 00:32:18 -0400

    remove commented out code

--- airflow/docker_image_test.go
+++ airflow/docker_image_test.go
@@ -203,32 +203,6 @@ func TestDockerImageListLabel(t *testing.T) {
 	})
 }
 
-// func TestDockerTagLocalImage(t *testing.T) {
-// 	handler := DockerImage{
-// 		imageName: "testing",
-// 	}
-
-// 	previousCmdExec := cmdExec
-
-// 	t.Run("rename local image success", func(t *testing.T) {
-// 		cmdExec = func(cmd string, stdout, stderr io.Writer, args ...string) error {
-// 			return nil
-// 		}
-// 		err := handler.TagLocalImage("custom-image")
-// 		assert.NoError(t, err)
-// 	})
-
-// 	t.Run("rename local image error", func(t *testing.T) {
-// 		cmdExec = func(cmd string, stdout, stderr io.Writer, args ...string) error {
-// 			return errMock
-// 		}
-// 		err := handler.TagLocalImage("custom-image")
-// 		assert.Contains(t, err.Error(), errMock.Error())
-// 	})
-
-// 	cmdExec = previousCmdExec
-// }
-
 func TestExecCmd(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		stdout := new(bytes.Buffer)
