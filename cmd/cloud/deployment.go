6da97239d6f1ac84169876c102b92e6889a440e2
David Koenitzer <davidkoenit@gmail.com>
2022-08-02 12:12:27 -0400

    Multiple key=value pairs to variable create and update (#660)
    * add var list feature
    
    * hide old flags
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix broken code
    
    * remove broken test
    
    * fix tests
    
    * remove comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Neel Dalsania <neel.dalsania@astronomer.io>
    
    * refactor function
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Neel Dalsania <neel.dalsania@astronomer.io>

--- cmd/cloud/deployment.go
+++ cmd/cloud/deployment.go
@@ -41,15 +41,15 @@ var (
 		`
 	deploymentVariableCreateExample = `
 		# Create a deployment variable
-		$ astro deployment variable create --deployment-id <deployment-id> --key FOO --value BAR --secret
+		$ astro deployment variable create FOO=BAR FOO2=BAR2 --deployment-id <deployment-id> --secret
 		# Create a deployment variables from a file
-		$ astro deployment variable create  --deployment-id <deployment-id> --load --env .env.my-deployment
+		$ astro deployment variable create --deployment-id <deployment-id> --load --env .env.my-deployment
 		`
 	deploymentVariableUpdateExample = `
 		# Update a deployment variable
-		$ astro deployment variable update --deployment-id <deployment-id> --key KEY --value NEWVALUE --secret
+		$ astro deployment variable update FOO=NEWBAR FOO2=NEWBAR2 --deployment-id <deployment-id> --secret
 		# Update a deployment variables from a file
-		$ astro deployment variable update  --deployment-id <deployment-id> --load --env .env.my-deployment
+		$ astro deployment variable update --deployment-id <deployment-id> --load --env .env.my-deployment
 		`
 
 	httpClient = httputil.NewHTTPClient()
@@ -189,10 +189,10 @@ func newDeploymentVariableListCmd(out io.Writer) *cobra.Command {
 // nolint:dupl
 func newDeploymentVariableCreateCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "create",
-		Short:   "Create Deployment-level environment variables",
-		Long:    "Create Deployment-level environment variables by supplying either a key and value or an environment file with a list of keys and values",
-		Args:    cobra.NoArgs,
+		Use:   "create [key1=val1 key2=val2]",
+		Short: "Create Deployment-level environment variables",
+		Long:  "Create Deployment-level environment variables by supplying either a key and value or an environment file with a list of keys and values",
+		// Args:    cobra.NoArgs,
 		Example: deploymentVariableCreateExample,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return deploymentVariableCreate(cmd, args, out)
@@ -204,6 +204,8 @@ func newDeploymentVariableCreateCmd(out io.Writer) *cobra.Command {
 	cmd.Flags().BoolVarP(&useEnvFile, "load", "l", false, "Create environment variables loaded from an environment file")
 	cmd.Flags().BoolVarP(&makeSecret, "secret", "s", false, "Set the new environment variables as secrets")
 	cmd.Flags().StringVarP(&envFile, "env", "e", ".env", "Location of file to load environment variables from")
+	_ = cmd.Flags().MarkHidden("key")
+	_ = cmd.Flags().MarkHidden("value")
 
 	return cmd
 }
@@ -211,10 +213,9 @@ func newDeploymentVariableCreateCmd(out io.Writer) *cobra.Command {
 // nolint:dupl
 func newDeploymentVariableUpdateCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "update",
+		Use:     "update [key1=update_val1 key2=update_val2]",
 		Short:   "Update Deployment-level environment variables",
 		Long:    "Update Deployment-level environment variables by supplying either a key and value or an environment file with a list of keys and values, variables that don't already exist will be created",
-		Args:    cobra.NoArgs,
 		Example: deploymentVariableUpdateExample,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return deploymentVariableUpdate(cmd, args, out)
@@ -226,6 +227,8 @@ func newDeploymentVariableUpdateCmd(out io.Writer) *cobra.Command {
 	cmd.Flags().BoolVarP(&useEnvFile, "load", "l", false, "Update environment variables loaded from an environment file")
 	cmd.Flags().BoolVarP(&makeSecret, "secret", "s", false, "Set updated environment variables as secrets")
 	cmd.Flags().StringVarP(&envFile, "env", "e", ".env", "Location of file to load environment variables to update from")
+	_ = cmd.Flags().MarkHidden("key")
+	_ = cmd.Flags().MarkHidden("value")
 
 	return cmd
 }
@@ -330,26 +333,30 @@ func deploymentVariableList(cmd *cobra.Command, _ []string, out io.Writer) error
 	return deployment.VariableList(deploymentID, variableKey, ws, envFile, useEnvFile, astroClient, out)
 }
 
-func deploymentVariableCreate(cmd *cobra.Command, _ []string, out io.Writer) error {
+func deploymentVariableCreate(cmd *cobra.Command, args []string, out io.Writer) error {
 	ws, err := coalesceWorkspace()
 	if err != nil {
 		return errors.Wrap(err, "failed to find a valid workspace")
 	}
 
+	variableList := args
+
 	// Silence Usage as we have now validated command input
 	cmd.SilenceUsage = true
 
-	return deployment.VariableModify(deploymentID, variableKey, variableValue, ws, envFile, useEnvFile, makeSecret, false, astroClient, out)
+	return deployment.VariableModify(deploymentID, variableKey, variableValue, ws, envFile, variableList, useEnvFile, makeSecret, false, astroClient, out)
 }
 
-func deploymentVariableUpdate(cmd *cobra.Command, _ []string, out io.Writer) error {
+func deploymentVariableUpdate(cmd *cobra.Command, args []string, out io.Writer) error {
 	ws, err := coalesceWorkspace()
 	if err != nil {
 		return errors.Wrap(err, "failed to find a valid workspace")
 	}
 
+	variableList := args
+
 	// Silence Usage as we have now validated command input
 	cmd.SilenceUsage = true
 
-	return deployment.VariableModify(deploymentID, variableKey, variableValue, ws, envFile, useEnvFile, makeSecret, true, astroClient, out)
+	return deployment.VariableModify(deploymentID, variableKey, variableValue, ws, envFile, variableList, useEnvFile, makeSecret, true, astroClient, out)
 }
