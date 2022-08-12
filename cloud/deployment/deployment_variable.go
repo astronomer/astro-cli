1f12426edacaa7d3ea1d255e17a30881bde21fd8
sunkickr <davidkoenit@gmail.com>
2022-08-12 10:07:58 -0400

    fix truncated env vars

--- cloud/deployment/deployment_variable.go
+++ cloud/deployment/deployment_variable.go
@@ -259,8 +259,12 @@ func addVariablesFromFile(envFile string, oldKeyList []string, oldEnvironmentVar
 		if vars[i] == "" {
 			continue
 		}
-		key := strings.Split(vars[i], "=")[0]
-		value := strings.Split(vars[i], "=")[1]
+		if len(strings.SplitN(vars[i], "=", 2)) == 1{
+			fmt.Printf("%s is an improperly formated varaible no varaible created",vars[i])
+			continue
+		}
+		key := strings.SplitN(vars[i], "=", 2)[0]
+		value := strings.SplitN(vars[i], "=", 2)[1]
 		if key == "" {
 			fmt.Printf("empty key! skipping creating variable with value: %s\n", value)
 			continue
