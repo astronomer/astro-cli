03d057f398fb77b06704b72382789d18e4148612
Jemish Patel <jemish.patel@astronomer.io>
2022-08-30 18:06:26 -0700

    worker queue delete command
    - command to delete existing worker queue
    - prompts user for queue name if `--name` was not provided
    - prevents user from deleting `default` queue
    - uses `-force` to confirm with user before deleting a queue
    - resolves #671

--- cmd/cloud/deployment_workerqueue.go
+++ cmd/cloud/deployment_workerqueue.go
@@ -14,6 +14,7 @@ var (
 	maxWorkerCount int
 	workerType     string
 	name           string
+	force          bool
 )
 
 func newDeploymentWorkerQueueRootCmd(out io.Writer) *cobra.Command {
@@ -25,6 +26,7 @@ func newDeploymentWorkerQueueRootCmd(out io.Writer) *cobra.Command {
 	}
 	cmd.AddCommand(
 		newDeploymentWorkerQueueCreateCmd(out),
+		newDeploymentWorkerQueueDeleteCmd(out),
 	)
 	return cmd
 }
@@ -39,14 +41,31 @@ func newDeploymentWorkerQueueCreateCmd(out io.Writer) *cobra.Command {
 			return deploymentWorkerQueueCreate(cmd, args, out)
 		},
 	}
-	cmd.Flags().StringVarP(&deploymentID, "deployment-id", "d", "", "The deployment where the worker queue should be created.")
-	cmd.Flags().StringVarP(&deploymentName, "deployment-name", "", "", "Name of the deployment where the worker queue should be created.")
-	cmd.Flags().StringVarP(&name, "name", "n", "", "The name of the worker queue. Queue names must not exceed 63 characters and contain only lowercase alphanumeric characters or '-' and start with an alphabetical character.")
+	cmd.Flags().StringVarP(&deploymentID, "deployment-id", "d", "", "The deployment where the worker queue should be deleted.")
+	cmd.Flags().StringVarP(&deploymentName, "deployment-name", "", "", "Name of the deployment where the worker queue should be deleted.")
+	cmd.Flags().StringVarP(&name, "name", "n", "", "The name of the worker queue to delete.")
 	cmd.Flags().IntVarP(&minWorkerCount, "min-count", "", 0, "The min worker count of the worker queue.")
 	cmd.Flags().IntVarP(&maxWorkerCount, "max-count", "", 0, "The max worker count of the worker queue.")
 	cmd.Flags().IntVarP(&concurrency, "concurrency", "", 0, "The concurrency(number of slots) of the worker queue.")
 	cmd.Flags().StringVarP(&workerType, "worker-type", "t", "", "The worker type of the default worker queue.")
 
+	return cmd
+}
+
+func newDeploymentWorkerQueueDeleteCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "delete",
+		Aliases: []string{"de"},
+		Short:   "Delete a Deployment's worker queue",
+		Long:    "Delete a worker queue from an Astro Deployment",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return deploymentWorkerQueueDelete(cmd, args, out)
+		},
+	}
+	cmd.Flags().StringVarP(&deploymentID, "deployment-id", "d", "", "The deployment where the worker queue should be created.")
+	cmd.Flags().StringVarP(&deploymentName, "deployment-name", "", "", "Name of the deployment where the worker queue should be created.")
+	cmd.Flags().StringVarP(&name, "name", "n", "", "The name of the worker queue. Queue names must not exceed 63 characters and contain only lowercase alphanumeric characters or '-' and start with an alphabetical character.")
+	cmd.Flags().BoolVarP(&force, "force", "f", false, "Force delete: Don't prompt a user for confirmation")
 	return cmd
 }
 
@@ -59,4 +78,14 @@ func deploymentWorkerQueueCreate(cmd *cobra.Command, _ []string, out io.Writer) 
 	}
 
 	return workerqueue.Create(ws, deploymentID, deploymentName, name, workerType, minWorkerCount, maxWorkerCount, concurrency, astroClient, out)
+}
+
+func deploymentWorkerQueueDelete(cmd *cobra.Command, _ []string, out io.Writer) error {
+	cmd.SilenceUsage = true
+
+	ws, err := coalesceWorkspace()
+	if err != nil {
+		return err
+	}
+	return workerqueue.Delete(ws, deploymentID, deploymentName, name, force, astroClient, out)
 }
