c11e43ec17b5ba3ef226ebd11cae1a8bdd935fa0
Pankaj Koti <pankajkoti699@gmail.com>
2022-10-20 21:11:03 +0530

    Gofumpt errors file

--- sql/errors.go
+++ sql/errors.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 )
 
-var errEnvVarNotSetError = errors.New("environment Variable not set")
-var errArgNotSetError = errors.New("argument not set")
+var (
+	errEnvVarNotSetError = errors.New("environment Variable not set")
+	errArgNotSetError    = errors.New("argument not set")
+)
 
 func EnvVarNotSetError(envVar string) error {
 	return fmt.Errorf("%w:%s", errEnvVarNotSetError, envVar)
