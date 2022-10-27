117fcae71195ddb018880c8a25b48c907f5c99d3
feluelle <felix.uellendall@astronomer.io>
2022-10-27 11:03:51 +0200

    Run cmd tests independently if possible

--- cmd/sql/flow_test.go
+++ cmd/sql/flow_test.go
@@ -13,27 +13,56 @@ func execFlowCmd(args ...string) error {
 	return err
 }
 
-func TestFlowCmd(t *testing.T) {
-	tempDir := t.TempDir()
-
+func TestFlowVersionCmd(t *testing.T) {
 	err := execFlowCmd([]string{"version"}...)
 	assert.NoError(t, err)
+}
 
-	err = execFlowCmd([]string{"about"}...)
+func TestFlowAboutCmd(t *testing.T) {
+	err := execFlowCmd([]string{"about"}...)
+	assert.NoError(t, err)
+}
+
+func TestFlowInitCmd(t *testing.T) {
+	tempDir := t.TempDir()
+	tempDirAirflowHome := t.TempDir()
+	tempDirAirflowDagsFolder := t.TempDir()
+	err := execFlowCmd([]string{"init", tempDir, "--airflow_home", tempDirAirflowHome, "--airflow_dags_folder", tempDirAirflowDagsFolder}...)
+	assert.NoError(t, err)
+}
+
+func TestFlowValidateCmd(t *testing.T) {
+	tempDir := t.TempDir()
+	err := execFlowCmd([]string{"init", tempDir}...)
 	assert.NoError(t, err)
 
-	err = execFlowCmd([]string{"init", tempDir}...)
+	err = execFlowCmd([]string{"validate", tempDir, "--connection", "sqlite_conn"}...)
 	assert.NoError(t, err)
+}
 
-	err = execFlowCmd([]string{"validate", "--project_dir", tempDir}...)
+func TestFlowGenerateCmd(t *testing.T) {
+	tempDir := t.TempDir()
+	err := execFlowCmd([]string{"init", tempDir}...)
 	assert.NoError(t, err)
 
 	err = execFlowCmd([]string{"generate", "example_basic_transform", "--project_dir", tempDir}...)
 	assert.NoError(t, err)
+}
 
-	err = execFlowCmd([]string{"run", "example_basic_transform", "--project_dir", tempDir}...)
+func TestFlowRunCmd(t *testing.T) {
+	tempDir := t.TempDir()
+	err := execFlowCmd([]string{"init", tempDir}...)
 	assert.NoError(t, err)
 
 	err = execFlowCmd([]string{"run", "example_templating", "--env", "dev", "--project_dir", tempDir}...)
 	assert.NoError(t, err)
+}
+
+func TestFlowRunCmdWorkflowNameNotSet(t *testing.T) {
+	tempDir := t.TempDir()
+	err := execFlowCmd([]string{"init", tempDir}...)
+	assert.NoError(t, err)
+
+	err = execFlowCmd([]string{"run"}...)
+	assert.EqualError(t, err, "argument not set:workflow_name")
 }
