2dfa5fc5cc40141bde81040f57261946fe879a9b
Pankaj Koti <pankajkoti699@gmail.com>
2022-11-02 00:22:18 +0530

    Check error contains substring

--- sql/pypi_client_test.go
+++ sql/pypi_client_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestGetPypiVersionInvalidHostFailure(t *testing.T) {
 	_, err := GetPypiVersion("http://abcd")
-	expectedErrMessage := "error getting latest release version for project url http://abcd,  Get \"http://abcd\": dial tcp: lookup abcd: no such host"
-	assert.EqualError(t, err, expectedErrMessage)
+	expectedErrContains := "error getting latest release version for project url http://abcd,  Get \"http://abcd\": dial tcp: lookup abcd"
+	assert.ErrorContains(t, err, expectedErrContains)
 }
