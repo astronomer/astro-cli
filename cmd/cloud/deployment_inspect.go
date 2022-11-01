1ecde814fa9d57fc63c437f4d7b0e86a551ccd6b
Jemish Patel <jemish.patel@astronomer.io>
2022-11-01 16:16:21 -0700

    add `--key` flag to inspect command
    - make deploymentID an arguement instead of a flag

--- cmd/cloud/deployment_inspect.go
+++ cmd/cloud/deployment_inspect.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var outputFormat string
+var outputFormat, requestedField string
 
 func newDeploymentInspectCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
@@ -20,18 +20,22 @@ func newDeploymentInspectCmd(out io.Writer) *cobra.Command {
 			return deploymentInspect(cmd, args, out)
 		},
 	}
-	cmd.Flags().StringVarP(&deploymentID, "deployment-id", "d", "", "The deployment to inspect.")
 	cmd.Flags().StringVarP(&deploymentName, "deployment-name", "n", "", "Name of the deployment to inspect.")
 	cmd.Flags().StringVarP(&outputFormat, "output", "o", "yaml", "Output format can be one of: yaml or json. By default the inspected deployment will be in YAML format.")
+	cmd.Flags().StringVarP(&requestedField, "key", "k", "", "A specific key for the deployment. Use --key configuration.cluster_id to get a deployment's cluster id.")
 	return cmd
 }
 
-func deploymentInspect(cmd *cobra.Command, _ []string, out io.Writer) error {
+func deploymentInspect(cmd *cobra.Command, args []string, out io.Writer) error {
 	cmd.SilenceUsage = true
 
 	wsID, err := coalesceWorkspace()
 	if err != nil {
 		return err
 	}
-	return inspect.Inspect(wsID, deploymentName, deploymentID, outputFormat, astroClient, out, "")
+
+	if len(args) > 0 {
+		deploymentID = args[0]
+	}
+	return inspect.Inspect(wsID, deploymentName, deploymentID, outputFormat, astroClient, out, requestedField)
 }
