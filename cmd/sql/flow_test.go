5fc8479ec34ed9f0b23bba9396ec1e85bc4d6de3
feluelle <felix.uellendall@astronomer.io>
2022-11-10 08:34:25 +0100

    Move commands to separate tests

--- cmd/sql/flow_test.go
+++ cmd/sql/flow_test.go
@@ -132,11 +132,23 @@ func TestFlowGenerateCmd(t *testing.T) {
 
 	err = execFlowCmd([]string{"generate", "example_basic_transform", "--project-dir", projectDir}...)
 	assert.NoError(t, err)
+}
 
-	err = execFlowCmd([]string{"generate", "example_basic_transform", "--generate-tasks", "--project-dir", projectDir}...)
+func TestFlowGenerateGenerateTasksCmd(t *testing.T) {
+	projectDir := t.TempDir()
+	err := execFlowCmd([]string{"init", projectDir}...)
 	assert.NoError(t, err)
 
-	err = execFlowCmd([]string{"run", "example_templating", "--env", "dev", "--generate-tasks", "--project-dir", projectDir, "--verbose"}...)
+	err = execFlowCmd([]string{"generate", "example_basic_transform", "--project-dir", projectDir, "--generate-tasks"}...)
+	assert.NoError(t, err)
+}
+
+func TestFlowRunGenerateTasksCmd(t *testing.T) {
+	projectDir := t.TempDir()
+	err := execFlowCmd([]string{"init", projectDir}...)
+	assert.NoError(t, err)
+
+	err = execFlowCmd([]string{"run", "example_basic_transform", "--project-dir", projectDir, "--generate-tasks"}...)
 	assert.NoError(t, err)
 }
 
