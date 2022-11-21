fd51f7fd90ade941d9074be1ff733d89c03783f6
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2022-11-21 22:42:00 +0000

    Add tests

--- sql/flow_config_test.go
+++ sql/flow_config_test.go
@@ -11,3 +11,15 @@ func TestGetPypiVersionInvalidHostFailure(t *testing.T) {
 	expectedErrContains := "error getting latest release version for project url http://abcd,  Get \"http://abcd\": dial tcp: lookup abcd"
 	assert.ErrorContains(t, err, expectedErrContains)
 }
+
+func TestGetBaseDockerImageURIInvalidURLFailure(t *testing.T) {
+	_, err := GetBaseDockerImageURI("http://efgh")
+	expectedErrContains := "error retrieving the latest configuration http://efgh,  Get \"http://efgh\": dial tcp: lookup efgh: no such host. Using the default"
+	assert.ErrorContains(t, err, expectedErrContains)
+}
+
+func TestGetBaseDockerImageURIInvalidJSONFailure(t *testing.T) {
+	_, err := GetBaseDockerImageURI("https://github.com/astronomer/astro-sdk/blob/main/README.md")
+	expectedErrContains := "error parsing the base docker image from the configuration file: invalid character '<' looking for beginning of value"
+	assert.ErrorContains(t, err, expectedErrContains)
+}
