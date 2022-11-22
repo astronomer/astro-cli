e6b73ff9d57df3949a0675eab650f1337782bc82
Felix Uellendall <feluelle@users.noreply.github.com>
2022-11-22 10:24:42 +0100

    Fix flow docker build (#892)
    * Fix flow docker build
    
    - add build-essential to system deps; note that this is required for snowflake-connector-python
    - forward build errors to user
    
    * Add test for JSONError
    
    * Use apt-install-and-clean
    
    - fix linting issues

--- sql/include/dockerfile_content.go
+++ sql/include/dockerfile_content.go
@@ -9,5 +9,9 @@ FROM %s
 
 ENV ASTRO_CLI Yes
 
+# build-essential is necessary to be able to build wheels for snowflake-connector-python
+RUN apt-install-and-clean \
+        build-essential
+
 RUN pip install astro-sql-cli==%s
 `)
