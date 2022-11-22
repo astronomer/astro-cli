cad3ed02a5404f98df23b385c50aa1c18e8918fb
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2022-11-22 00:17:13 +0000

    Improve the performance to build the SQL CLI image from 6 min to 1 (#882)
    * Improve the performance to build the SQL CLI image from 6 min to 1
    
    * Address code review feedback
    
    * Add tests
    
    * Fix tests
    
    * Improve tests coverage

---
+++ sql/flow_config.go
@@ -0,0 +1,69 @@
+package sql
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type pypiVersionResponse struct {
+	Info struct {
+		Version string `json:"version"`
+	} `json:"info"`
+}
+
+type configResponse struct {
+	BaseDockerImage string `json:"baseDockerImage"`
+}
+
+const (
+	defaultDockerImageURI = "quay.io/astronomer/astro-runtime:6.0.4-base"
+)
+
+var (
+	getPypiVersion        = GetPypiVersion
+	getBaseDockerImageURI = GetBaseDockerImageURI
+)
+
+func GetPypiVersion(projectURL string) (string, error) {
+	httpClient := &http.Client{}
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodGet, projectURL, http.NoBody)
+	if err != nil {
+		return "", fmt.Errorf("error creating HTTP request %w", err)
+	}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error getting latest release version for project url %s,  %w", projectURL, err)
+	}
+	defer res.Body.Close()
+
+	var resp pypiVersionResponse
+	err = json.NewDecoder(res.Body).Decode(&resp)
+	if err != nil {
+		return "", fmt.Errorf("error parsing response for project version %w", err)
+	}
+
+	return resp.Info.Version, nil
+}
+
+func GetBaseDockerImageURI(configURL string) (string, error) {
+	httpClient := &http.Client{}
+	req, err := http.NewRequestWithContext(context.TODO(), http.MethodGet, configURL, http.NoBody)
+	if err != nil {
+		return defaultDockerImageURI, fmt.Errorf("error creating HTTP request %w. Using the default", err)
+	}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return defaultDockerImageURI, fmt.Errorf("error retrieving the latest configuration %s,  %w. Using the default", configURL, err)
+	}
+	defer res.Body.Close()
+
+	var resp configResponse
+	err = json.NewDecoder(res.Body).Decode(&resp)
+	if err != nil {
+		return defaultDockerImageURI, fmt.Errorf("error parsing the base docker image from the configuration file: %w", err)
+	}
+
+	return resp.BaseDockerImage, nil
+}
--- sql/include/dockerfile_content.go
+++ sql/include/dockerfile_content.go
@@ -5,11 +5,9 @@ import "strings"
 // Dockerfile is the Dockerfile template
 
 var Dockerfile = strings.TrimSpace(`
-FROM python:%s-slim-bullseye
+FROM %s
 
 ENV ASTRO_CLI Yes
-
-RUN apt-get update -y && apt-get install -y gcc g++ libpq-dev python-dev libssl-dev libffi-dev
 
 RUN pip install astro-sql-cli==%s
 `)
