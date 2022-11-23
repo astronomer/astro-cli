9e48aa794363409217e3ff56cf6b31b904f5bbd0
Felix Uellendall <feluelle@users.noreply.github.com>
2022-11-23 13:23:52 +0100

    Disable astronomer update-check in flow env (#895)

--- sql/include/dockerfile_content.go
+++ sql/include/dockerfile_content.go
@@ -8,6 +8,7 @@ var Dockerfile = strings.TrimSpace(`
 FROM %s
 
 ENV ASTRO_CLI Yes
+ENV AIRFLOW__ASTRONOMER__UPDATE_CHECK_INTERVAL 0
 
 # build-essential is necessary to be able to build wheels for snowflake-connector-python
 RUN apt-install-and-clean \
