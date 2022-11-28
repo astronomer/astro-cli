9550c86e8ad25e4df9c28662f383a8a484d76359
sunkickr <davidkoenit@gmail.com>
2022-11-28 16:44:25 -0500

    fix test

--- airflow/docker_test.go
+++ airflow/docker_test.go
@@ -108,7 +108,7 @@ services:
       - airflow_home/plugins:/usr/local/airflow/plugins:z
       - airflow_home/include:/usr/local/airflow/include:z
       - airflow_home/tests:/usr/local/airflow/tests:z
-
+	  
       - airflow_logs:/usr/local/airflow/logs
     
 
@@ -219,6 +219,7 @@ services:
       - airflow_home/plugins:/usr/local/airflow/plugins:z
       - airflow_home/include:/usr/local/airflow/include:z
       - airflow_home/tests:/usr/local/airflow/tests:z
+
       - airflow_logs:/usr/local/airflow/logs
     
 
