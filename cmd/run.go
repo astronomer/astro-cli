fc4e4ba13e63f3e54a5b45994150bdf5bffe5d88
sunkickr <davidkoenit@gmail.com>
2022-11-29 11:23:30 -0500

    astro run copy change

--- cmd/run.go
+++ cmd/run.go
@@ -15,7 +15,7 @@ func newRunCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "run DAG-ID",
 		Short: "Run a local DAG with python by running its tasks sequentially",
-		Long:  "Run a local DAG by running its tasks sequentially. This command will spin up a docker airflow environment and execute your DAG code",
+		Long:  "Run a local DAG by running its tasks sequentially. This command will spin up a docker airflow environment and execute your DAG code. It will parse all the files in your dags folder. Remove files you do not wish to be parsed.",		
 		Args:  cobra.ExactArgs(1),
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			return nil
@@ -25,7 +25,7 @@ func newRunCommand() *cobra.Command {
 	}
 	cmd.Flags().StringVarP(&envFile, "env", "e", ".env", "Location of file containing environment variables")
 	cmd.Flags().BoolVarP(&noCache, "no-cache", "", false, "Do not use cache when building container image")
-	cmd.Flags().StringVarP(&settingsFile, "settings-file", "s", "airflow_settings.yaml", "Settings file from which to import airflow objects")
+	cmd.Flags().StringVarP(&settingsFile, "settings-file", "s", "airflow_settings.yaml", "Settings file from which to import airflow objects. If you create this file while airflow is running make sure restart airflow")
 
 	return cmd
 }
