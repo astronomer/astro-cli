37d81859b75567d877c2cf1d1fcba9d40c835d4b
Kaxil Naik <kaxilnaik@gmail.com>
2022-12-15 21:42:19 +0000

    Expose configs on Webserver for local dev (#958)
    We should expose configs on the webserver to aid debugging, this comes in handy where trying to figure out if configs set via env var took effect or not. There is no reason to hide them for local dev.
    
    closes #959

--- airflow/include/composeyml.go
+++ airflow/include/composeyml.go
@@ -14,6 +14,7 @@ x-common-env-vars: &common-env-vars
   AIRFLOW__CORE__FERNET_KEY: "d6Vefz3G9U_ynXB3cr7y_Ak35tAHkEGAVxuz_B-jzWw="
   AIRFLOW__WEBSERVER__SECRET_KEY: "{{ .ProjectName }}"
   AIRFLOW__WEBSERVER__RBAC: "True"
+  AIRFLOW__WEBSERVER__EXPOSE_CONFIG: "True"
   ASTRONOMER_ENVIRONMENT: local
 
 networks:
