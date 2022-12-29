d79bfb1d9ae03b11d63238294160b80cee924774
sunkickr <davidkoenit@gmail.com>
2022-12-29 14:10:09 -0500

    add user list and update commands

--- cloud/user/user.go
+++ cloud/user/user.go
@@ -4,10 +4,15 @@ import (
 	httpContext "context"
 	"fmt"
 	"io"
+	"os"
+	"time"
+	"strconv"
 
 	astrocore "github.com/astronomer/astro-cli/astro-client-core"
 	"github.com/astronomer/astro-cli/config"
 	"github.com/astronomer/astro-cli/context"
+	"github.com/astronomer/astro-cli/pkg/input"
+	"github.com/astronomer/astro-cli/pkg/printutil"
 
 	"github.com/pkg/errors"
 )
@@ -16,6 +21,8 @@ var (
 	ErrNoShortName  = errors.New("cannot retrieve organization short name from context")
 	ErrInvalidRole  = errors.New("requested role is invalid. Possible values are ORGANIZATION_MEMBER, ORGANIZATION_BILLING_ADMIN and ORGANIZATION_OWNER ")
 	ErrInvalidEmail = errors.New("no email provided for the invite. Retry with a valid email address")
+	ErrInvalidUserKey = errors.New("invalid User selected")
+
 )
 
 // CreateInvite calls the CreateUserInvite mutation to create a user invite
@@ -55,6 +62,53 @@ func CreateInvite(email, role string, out io.Writer, client astrocore.CoreClient
 	return nil
 }
 
+func UpdateUserRole(email, role string, out io.Writer, client astrocore.CoreClient) error {
+	var userID string
+	err := IsRoleValid(role)
+	if err != nil {
+		return err
+	}
+	ctx, err := context.GetCurrentContext()
+	if err != nil {
+		return err
+	}
+	if ctx.OrganizationShortName == "" {
+		return ErrNoShortName
+	}
+	// Get all org users. Setting limit to 1000 for now
+	users, err := GetOrgUsers(client, 1000)
+	if err != nil {
+		return err
+	}
+	if email == "" {
+		user, err := selectUser(users, out)
+		userID = user.Id
+		email = user.Username
+		if err != nil {
+			return err
+		}
+	} else {
+		for _, user := range users {
+			if user.Username == email {
+				userID = user.Id
+			}
+		}
+	}
+	mutateUserInput := astrocore.MutateOrgUserRoleRequest{
+		Role: role,
+	}
+	resp, err := client.MutateOrgUserRoleWithResponse(httpContext.Background(), ctx.OrganizationShortName, userID, mutateUserInput)
+	if err != nil {
+		return err
+	}
+	err = astrocore.NormalizeAPIError(resp.HTTPResponse, resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(out, "The user %s role was successfully updated to %s\n", email, role)
+	return nil
+}
+
 // IsRoleValid checks if the requested role is valid
 // If the role is valid, it returns nil
 // error errInvalidRole is returned if the role is not valid
@@ -67,3 +121,104 @@ func IsRoleValid(role string) error {
 	}
 	return ErrInvalidRole
 }
+
+func selectUser(users []astrocore.User, out io.Writer) (astrocore.User, error) {
+	table := printutil.Table{
+		Padding:        []int{30, 50, 10, 50, 10, 10, 10},
+		DynamicPadding: true,
+		Header:         []string{"#", "FULLNAME", "EMAIL", "ID", "ORGANIZATION ROLE", "CREATE DATE"},
+	}
+
+	fmt.Println("\nPlease select the user who's role you would like to update:")
+
+	// sort.Slice(deployments, func(i, j int) bool {
+	// 	return deployments[i].CreatedAt.Before(deployments[j].CreatedAt)
+	// })
+
+	userMap := map[string]astrocore.User{}
+	for i := range users {
+		index := i + 1
+		table.AddRow([]string{
+			strconv.Itoa(index), 
+			users[i].FullName,
+			users[i].Username,
+			users[i].Id,
+			*users[i].OrgRole,
+			users[i].CreatedAt.Format(time.RFC3339),
+		}, false)
+
+		userMap[strconv.Itoa(index)] = users[i]
+	}
+
+	table.Print(os.Stdout)
+	choice := input.Text("\n> ")
+	selected, ok := userMap[choice]
+	if !ok {
+		return astrocore.User{}, ErrInvalidUserKey
+	}
+	return selected, nil
+}
+
+// Returns a list of all of an organizations users
+func GetOrgUsers(client astrocore.CoreClient, limit int) ([]astrocore.User, error) {
+	offset := 0
+	var users []astrocore.User
+
+	ctx, err := context.GetCurrentContext()
+	if err != nil {
+		return nil, err
+	}
+	if ctx.OrganizationShortName == "" {
+		return nil, ErrNoShortName
+	}
+
+	for {
+		resp, err := client.ListOrgUsersWithResponse(httpContext.Background(), ctx.OrganizationShortName, &astrocore.ListOrgUsersParams{
+		Offset: &offset,
+		Limit: &limit,
+	})
+		if err != nil {
+			return nil, err
+		}
+		err = astrocore.NormalizeAPIError(resp.HTTPResponse, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, resp.JSON200.Users...)
+
+		if resp.JSON200.TotalCount <= offset {
+			break
+		}
+
+		offset += limit
+	}
+
+	return users, nil
+}
+
+// Prints a list of all of an organizations users
+func ListOrgUsers(out io.Writer, client astrocore.CoreClient, limit int) error {
+	table := printutil.Table{
+		Padding:        []int{30, 50, 10, 50, 10, 10, 10},
+		DynamicPadding: true,
+		Header:         []string{"FULLNAME", "EMAIL", "ID", "ORGANIZATION ROLE", "CREATE DATE"},
+	}
+	users, err := GetOrgUsers(client, limit)
+	if err != nil {
+		return err
+	}
+
+	for i := range users {
+		table.AddRow([]string{
+			users[i].FullName,
+			users[i].Username,
+			users[i].Id,
+			*users[i].OrgRole,
+			users[i].CreatedAt.Format(time.RFC3339),
+		}, false)
+	}
+
+	table.Print(out)
+	return nil
+}
+
--- cmd/cloud/user.go
+++ cmd/cloud/user.go
@@ -9,7 +9,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var role string
+var (
+	role string
+	limit int
+)
 
 func newUserCmd(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
@@ -21,6 +24,8 @@ func newUserCmd(out io.Writer) *cobra.Command {
 	cmd.SetOut(out)
 	cmd.AddCommand(
 		newUserInviteCmd(out),
+		newUserListCmd(out),
+		newUserUpdateCmd(out),
 	)
 	return cmd
 }
@@ -41,6 +46,36 @@ func newUserInviteCmd(out io.Writer) *cobra.Command {
 	return cmd
 }
 
+func newUserListCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "list",
+		Aliases: []string{"inv"},
+		Short:   "List all the users in your Astro Organization",
+		Long: "List all the users in your Astro Organization",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return listUsers(cmd, args, out)
+		},
+	}
+	cmd.Flags().IntVarP(&limit, "limit", "l", 100, "Maximum number of organization users listed")
+	return cmd
+}
+
+func newUserUpdateCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "update [email]",
+		Aliases: []string{"inv"},
+		Short:   "Update a the role of a user your in Astro Organization",
+		Long: "Update the role of a user in your Astro Organization\n$astro user update [email] --role [ORGANIZATION_MEMBER, " +
+			"ORGANIZATION_BILLING_ADMIN, ORGANIZATION_OWNER].",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return userUpdate(cmd, args, out)
+		},
+	}
+	cmd.Flags().StringVarP(&role, "role", "r", "ORGANIZATION_MEMBER", "The new role for the "+
+		"user. Possible values are ORGANIZATION_MEMBER, ORGANIZATION_BILLING_ADMIN and ORGANIZATION_OWNER ")
+	return cmd
+}
+
 func userInvite(cmd *cobra.Command, args []string, out io.Writer) error {
 	var email string
 
@@ -54,4 +89,21 @@ func userInvite(cmd *cobra.Command, args []string, out io.Writer) error {
 
 	cmd.SilenceUsage = true
 	return user.CreateInvite(email, role, out, astroCoreClient)
+}
+
+func listUsers(cmd *cobra.Command, args []string, out io.Writer) error {
+	cmd.SilenceUsage = true
+	return user.ListOrgUsers(out, astroCoreClient, limit)
+}
+
+func userUpdate(cmd *cobra.Command, args []string, out io.Writer) error {
+	var email string
+
+	// if an email was provided in the args we use it
+	if len(args) > 0 {
+		email = args[0]
+	}
+
+	cmd.SilenceUsage = true
+	return user.UpdateUserRole(email, role, out, astroCoreClient)
 }
