b412c985df74aedbe06d7765fad8580b0942b215
sunkickr <davidkoenit@gmail.com>
2023-01-03 10:36:44 -0500

    fix lint

--- cmd/root.go
+++ cmd/root.go
@@ -72,7 +72,7 @@ Welcome to the Astro CLI, the modern command line interface for data orchestrati
 				// compare current version to latest
 				err = version.CompareVersions(githubClient, "astronomer", "astro-cli")
 				if err != nil {
-					softwareCmd.InitDebugLogs = append(softwareCmd.InitDebugLogs, fmt.Errorf("Error comparing CLI versions: %s", err.Error()))
+					softwareCmd.InitDebugLogs = append(softwareCmd.InitDebugLogs, "Error comparing CLI versions: " + err.Error())
 				}
 			}
 			if isCloudCtx {
