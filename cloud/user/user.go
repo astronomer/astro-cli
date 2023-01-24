59220786f4255c9ebef727e44335f22a2f4a9261
David Koenitzer <davidkoenit@gmail.com>
2023-01-24 15:53:26 -0500

    add user list and update commands (#990)
    * add user list and update commands
    
    * move fmt
    
    * fix lint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add tests
    
    * fix lint
    
    * add tests
    
    * fix lint
    
    * add test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix test
    
    * Apply suggestions from code review
    
    Co-authored-by: Neel Dalsania <neel.dalsania@astronomer.io>
    
    * moved user commands under organization
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix lint
    
    * fix pre-commit
    
    * update mocks
    
    * remove unneeded file
    
    * Update cmd/cloud/organization.go
    
    Co-authored-by: kushalmalani <kushal@astronomer.io>
    
    * fix syntax
    
    * fix syntax
    
    * remove github.com/pkg/errors
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix test
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Neel Dalsania <neel.dalsania@astronomer.io>
    Co-authored-by: kushalmalani <kushal@astronomer.io>

--- cloud/user/user.go
+++ cloud/user/user.go
@@ -4,18 +4,26 @@ import (
 	httpContext "context"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"time"
 
 	astrocore "github.com/astronomer/astro-cli/astro-client-core"
 	"github.com/astronomer/astro-cli/config"
 	"github.com/astronomer/astro-cli/context"
+	"github.com/astronomer/astro-cli/pkg/input"
+	"github.com/astronomer/astro-cli/pkg/printutil"
 
 	"github.com/pkg/errors"
 )
 
 var (
-	ErrNoShortName  = errors.New("cannot retrieve organization short name from context")
-	ErrInvalidRole  = errors.New("requested role is invalid. Possible values are ORGANIZATION_MEMBER, ORGANIZATION_BILLING_ADMIN and ORGANIZATION_OWNER ")
-	ErrInvalidEmail = errors.New("no email provided for the invite. Retry with a valid email address")
+	ErrNoShortName     = errors.New("cannot retrieve organization short name from context")
+	ErrInvalidRole     = errors.New("requested role is invalid. Possible values are ORGANIZATION_MEMBER, ORGANIZATION_BILLING_ADMIN and ORGANIZATION_OWNER ")
+	ErrInvalidEmail    = errors.New("no email provided for the invite. Retry with a valid email address")
+	ErrInvalidUserKey  = errors.New("invalid User selected")
+	userPagnationLimit = 100
+	ErrUserNotFound    = errors.New("no user was found for the email you provided")
 )
 
 // CreateInvite calls the CreateUserInvite mutation to create a user invite
@@ -55,6 +63,60 @@ func CreateInvite(email, role string, out io.Writer, client astrocore.CoreClient
 	return nil
 }
 
+func UpdateUserRole(email, role string, out io.Writer, client astrocore.CoreClient) error {
+	var userID string
+	err := IsRoleValid(role)
+	if err != nil {
+		return err
+	}
+	ctx, err := context.GetCurrentContext()
+	if err != nil {
+		return err
+	}
+	if ctx.OrganizationShortName == "" {
+		return ErrNoShortName
+	}
+	// Get all org users
+	users, err := GetOrgUsers(client)
+	if err != nil {
+		return err
+	}
+	if email != "" {
+		if err != nil {
+			return err
+		}
+
+		for i := range users {
+			if users[i].Username == email {
+				userID = users[i].Id
+			}
+		}
+		if userID == "" {
+			return ErrUserNotFound
+		}
+	} else {
+		user, err := selectUser(users)
+		userID = user.Id
+		email = user.Username
+		if err != nil {
+			return err
+		}
+	}
+	mutateUserInput := astrocore.MutateOrgUserRoleRequest{
+		Role: role,
+	}
+	resp, err := client.MutateOrgUserRoleWithResponse(httpContext.Background(), ctx.OrganizationShortName, userID, mutateUserInput)
+	if err != nil {
+		return err
+	}
+	err = astrocore.NormalizeAPIError(resp.HTTPResponse, resp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(out, "The user %s role was successfully updated to %s\n", email, role)
+	return nil
+}
+
 // IsRoleValid checks if the requested role is valid
 // If the role is valid, it returns nil
 // error errInvalidRole is returned if the role is not valid
@@ -66,4 +128,100 @@ func IsRoleValid(role string) error {
 		}
 	}
 	return ErrInvalidRole
+}
+
+func selectUser(users []astrocore.User) (astrocore.User, error) {
+	table := printutil.Table{
+		Padding:        []int{30, 50, 10, 50, 10, 10, 10},
+		DynamicPadding: true,
+		Header:         []string{"#", "FULLNAME", "EMAIL", "ID", "ORGANIZATION ROLE", "CREATE DATE"},
+	}
+
+	fmt.Println("\nPlease select the user who's role you would like to update:")
+
+	userMap := map[string]astrocore.User{}
+	for i := range users {
+		index := i + 1
+		table.AddRow([]string{
+			strconv.Itoa(index),
+			users[i].FullName,
+			users[i].Username,
+			users[i].Id,
+			*users[i].OrgRole,
+			users[i].CreatedAt.Format(time.RFC3339),
+		}, false)
+
+		userMap[strconv.Itoa(index)] = users[i]
+	}
+
+	table.Print(os.Stdout)
+	choice := input.Text("\n> ")
+	selected, ok := userMap[choice]
+	if !ok {
+		return astrocore.User{}, ErrInvalidUserKey
+	}
+	return selected, nil
+}
+
+// Returns a list of all of an organizations users
+func GetOrgUsers(client astrocore.CoreClient) ([]astrocore.User, error) {
+	offset := 0
+	var users []astrocore.User
+
+	ctx, err := context.GetCurrentContext()
+	if err != nil {
+		return nil, err
+	}
+	if ctx.OrganizationShortName == "" {
+		return nil, ErrNoShortName
+	}
+
+	for {
+		resp, err := client.ListOrgUsersWithResponse(httpContext.Background(), ctx.OrganizationShortName, &astrocore.ListOrgUsersParams{
+			Offset: &offset,
+			Limit:  &userPagnationLimit,
+		})
+		if err != nil {
+			return nil, err
+		}
+		err = astrocore.NormalizeAPIError(resp.HTTPResponse, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, resp.JSON200.Users...)
+
+		if resp.JSON200.TotalCount <= offset {
+			break
+		}
+
+		offset += userPagnationLimit
+	}
+
+	return users, nil
+}
+
+// Prints a list of all of an organizations users
+func ListOrgUsers(out io.Writer, client astrocore.CoreClient) error {
+	table := printutil.Table{
+		Padding:        []int{30, 50, 10, 50, 10, 10, 10},
+		DynamicPadding: true,
+		Header:         []string{"FULLNAME", "EMAIL", "ID", "ORGANIZATION ROLE", "CREATE DATE"},
+	}
+	users, err := GetOrgUsers(client)
+	if err != nil {
+		return err
+	}
+
+	for i := range users {
+		table.AddRow([]string{
+			users[i].FullName,
+			users[i].Username,
+			users[i].Id,
+			*users[i].OrgRole,
+			users[i].CreatedAt.Format(time.RFC3339),
+		}, false)
+	}
+
+	table.Print(out)
+	return nil
 }
