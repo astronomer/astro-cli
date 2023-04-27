0f6e5515da147c6b3cdc5400ae15378534bbdcd6
David Koenitzer <davidkoenit@gmail.com>
2023-04-26 21:05:31 -0400

    fix lint

--- cmd/cloud/deployment_inspect_test.go
+++ cmd/cloud/deployment_inspect_test.go
@@ -12,95 +12,93 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var (
-	deploymentResponse = []astro.Deployment{
-		{
-			ID:          "test-deployment-id",
-			Label:       "test-deployment-label",
-			ReleaseName: "great-release-name",
-			Workspace:   astro.Workspace{ID: "test-ws-id"},
-			Cluster: astro.Cluster{
-				ID: "cluster-id",
-				NodePools: []astro.NodePool{
-					{
-						ID:               "test-pool-id",
-						IsDefault:        false,
-						NodeInstanceType: "test-instance-type",
-						CreatedAt:        time.Now(),
-					},
-					{
-						ID:               "test-pool-id-1",
-						IsDefault:        true,
-						NodeInstanceType: "test-instance-type-1",
-						CreatedAt:        time.Now(),
-					},
-				},
-			},
-			RuntimeRelease: astro.RuntimeRelease{Version: "6.0.0", AirflowVersion: "2.4.0"},
-			DeploymentSpec: astro.DeploymentSpec{
-				Executor: "CeleryExecutor",
-				Scheduler: astro.Scheduler{
-					AU:       5,
-					Replicas: 3,
-				},
-				Webserver: astro.Webserver{URL: "some-url"},
-			},
-			WorkerQueues: []astro.WorkerQueue{
+var deploymentResponse = []astro.Deployment{
+	{
+		ID:          "test-deployment-id",
+		Label:       "test-deployment-label",
+		ReleaseName: "great-release-name",
+		Workspace:   astro.Workspace{ID: "test-ws-id"},
+		Cluster: astro.Cluster{
+			ID: "cluster-id",
+			NodePools: []astro.NodePool{
 				{
-					ID:                "test-wq-id",
-					Name:              "default",
-					IsDefault:         true,
-					MaxWorkerCount:    130,
-					MinWorkerCount:    12,
-					WorkerConcurrency: 110,
-					NodePoolID:        "test-pool-id",
+					ID:               "test-pool-id",
+					IsDefault:        false,
+					NodeInstanceType: "test-instance-type",
+					CreatedAt:        time.Now(),
 				},
 				{
-					ID:                "test-wq-id-1",
-					Name:              "test-queue-1",
-					IsDefault:         false,
-					MaxWorkerCount:    175,
-					MinWorkerCount:    8,
-					WorkerConcurrency: 150,
-					NodePoolID:        "test-pool-id-1",
-				},
-			},
-			UpdatedAt: time.Now(),
-			Status:    "HEALTHY",
-		},
-		{
-			ID:             "test-deployment-id-1",
-			Label:          "test-deployment-label-1",
-			RuntimeRelease: astro.RuntimeRelease{Version: "4.2.5"},
-			DeploymentSpec: astro.DeploymentSpec{
-				Scheduler: astro.Scheduler{
-					AU:       5,
-					Replicas: 3,
-				},
-			},
-			WorkerQueues: []astro.WorkerQueue{
-				{
-					ID:                "test-wq-id-2",
-					Name:              "test-queue-2",
-					IsDefault:         false,
-					MaxWorkerCount:    130,
-					MinWorkerCount:    12,
-					WorkerConcurrency: 110,
-					NodePoolID:        "test-nodepool-id-2",
-				},
-				{
-					ID:                "test-wq-id-3",
-					Name:              "test-queue-3",
-					IsDefault:         true,
-					MaxWorkerCount:    175,
-					MinWorkerCount:    8,
-					WorkerConcurrency: 150,
-					NodePoolID:        "test-nodepool-id-3",
+					ID:               "test-pool-id-1",
+					IsDefault:        true,
+					NodeInstanceType: "test-instance-type-1",
+					CreatedAt:        time.Now(),
 				},
 			},
 		},
-	}
-)
+		RuntimeRelease: astro.RuntimeRelease{Version: "6.0.0", AirflowVersion: "2.4.0"},
+		DeploymentSpec: astro.DeploymentSpec{
+			Executor: "CeleryExecutor",
+			Scheduler: astro.Scheduler{
+				AU:       5,
+				Replicas: 3,
+			},
+			Webserver: astro.Webserver{URL: "some-url"},
+		},
+		WorkerQueues: []astro.WorkerQueue{
+			{
+				ID:                "test-wq-id",
+				Name:              "default",
+				IsDefault:         true,
+				MaxWorkerCount:    130,
+				MinWorkerCount:    12,
+				WorkerConcurrency: 110,
+				NodePoolID:        "test-pool-id",
+			},
+			{
+				ID:                "test-wq-id-1",
+				Name:              "test-queue-1",
+				IsDefault:         false,
+				MaxWorkerCount:    175,
+				MinWorkerCount:    8,
+				WorkerConcurrency: 150,
+				NodePoolID:        "test-pool-id-1",
+			},
+		},
+		UpdatedAt: time.Now(),
+		Status:    "HEALTHY",
+	},
+	{
+		ID:             "test-deployment-id-1",
+		Label:          "test-deployment-label-1",
+		RuntimeRelease: astro.RuntimeRelease{Version: "4.2.5"},
+		DeploymentSpec: astro.DeploymentSpec{
+			Scheduler: astro.Scheduler{
+				AU:       5,
+				Replicas: 3,
+			},
+		},
+		WorkerQueues: []astro.WorkerQueue{
+			{
+				ID:                "test-wq-id-2",
+				Name:              "test-queue-2",
+				IsDefault:         false,
+				MaxWorkerCount:    130,
+				MinWorkerCount:    12,
+				WorkerConcurrency: 110,
+				NodePoolID:        "test-nodepool-id-2",
+			},
+			{
+				ID:                "test-wq-id-3",
+				Name:              "test-queue-3",
+				IsDefault:         true,
+				MaxWorkerCount:    175,
+				MinWorkerCount:    8,
+				WorkerConcurrency: 150,
+				NodePoolID:        "test-nodepool-id-3",
+			},
+		},
+	},
+}
 
 func TestNewDeploymentInspectCmd(t *testing.T) {
 	expectedHelp := "Inspect an Astro Deployment."
