8107ddf20f34c54c3f9256ccedf95d6574774340
Kushal Malani <kushal@astronomer.io>
2023-09-22 08:26:21 -0700

    Updating log print

--- godownloader.sh
+++ godownloader.sh
@@ -100,7 +100,7 @@ tag_to_version() {
     log_info "checking GitHub for tag '${TAG}'"
     REALTAG=$(github_release "$OWNER/$REPO" "${TAG}") && true
     if test -z "$REALTAG"; then
-      log_crit "unable to find '${TAG}' - use 'latest' or see https://github.com/${PREFIX}/releases for details"
+      log_crit "unable to find '${TAG}' - see https://github.com/${PREFIX}/releases for details"
       exit 1
     fi
     # if version starts with 'v', remove it
