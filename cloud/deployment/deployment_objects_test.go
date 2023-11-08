99efe0e9c84fbd64f0b73c028d48b236fd0737b9
Kushal Malani <kushal@astronomer.io>
2023-11-08 10:48:09 -0800

    Fix url in test

--- cloud/deployment/deployment_objects_test.go
+++ cloud/deployment/deployment_objects_test.go
@@ -93,8 +93,8 @@ func TestConnectionUpdate(t *testing.T) {
 }
 
 var (
-	fromAirflowURL = "from_airflow_url"
-	toAirflowURL   = "to_airflow_url"
+	fromAirflowURL = "from_airflow_url?orgID=orgId"
+	toAirflowURL   = "to_airflow_url?orgID=orgId"
 
 	fromConnections = []airflowclient.Connection{
 		{ConnID: "conn1", ConnType: "type1", Description: "desc1"},
