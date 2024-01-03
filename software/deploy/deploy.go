40bc196112f4133212c4376389aa8ac4a867ed74
Rujhan Arora <rujhan.arora@astronomer.io>
2024-01-03 16:13:01 +0530

    Added software version check for dag-only flag

--- software/deploy/deploy.go
+++ software/deploy/deploy.go
@@ -33,7 +33,7 @@ var (
 	errInvalidDeploymentID                  = errors.New("please specify a valid deployment ID")
 	errDeploymentNotFound                   = errors.New("no airflow deployments found")
 	errInvalidDeploymentSelected            = errors.New("invalid deployment selection\n") //nolint
-	ErrDagOnlyDeployDisabledInConfig        = errors.New("to perform this operation, use software version >= 0.34.0 and set both deployments.dagOnlyDeployment and deployments.configureDagDeployment to true in the Astronomer Platform")
+	ErrDagOnlyDeployDisabledInConfig        = errors.New("to perform this operation, set both deployments.dagOnlyDeployment and deployments.configureDagDeployment to true in the Astronomer Platform")
 	errDagOnlyDeployNotEnabledForDeployment = errors.New("to perform this operation, first set the deployment type to 'dag_only' via the UI or the API")
 )
 
