3e8678b5b74f341b8df9dde938aeb835ff59bc2c
David Koenitzer <davidkoenit@gmail.com>
2024-04-22 15:25:54 -0400

    remove input checks

--- cmd/errors.go
+++ cmd/errors.go
@@ -5,11 +5,9 @@ import (
 )
 
 var (
-	errInvalidBothAirflowAndRuntimeVersions        = errors.New("You provided both a runtime version and an Airflow version. You have to provide only one of these to initialize your project.") //nolint
-	errInvalidBothAirflowAndRuntimeVersionsUpgrade = errors.New("You provided both a runtime version and an Airflow version. You have to provide only one of these to upgrade.")                 //nolint
-	errInvalidBothDeploymentIDandCustomImage       = errors.New("You provided both a Deployment ID and a Custom image. You have to provide only one of these to upgrade.")                       //nolint
-	errInvalidBothDeploymentIDandVersion           = errors.New("You provided both a Deployment ID and a version. You have to provide only one of these to upgrade.")                            //nolint
-	errInvalidBothCustomImageandVersion            = errors.New("You provided both a Custom image and a version. You have to provide only one of these to upgrade.")                             //nolint
+	errInvalidBothAirflowAndRuntimeVersions        = errors.New("you provided both a runtime version and an Airflow version. You have to provide only one of these to initialize your project") //nolint
+	errInvalidBothAirflowAndRuntimeVersionsUpgrade = errors.New("you provided both a runtime version and an Airflow version. You have to provide only one of these to upgrade")                 //nolint
+	errInvalidBothCustomImageandVersion            = errors.New("you provided both a Custom image and a version. You have to provide only one of these to upgrade")                             //nolint
 
 	errConfigProjectName = errors.New("project name is invalid")
 	errProjectNameSpaces = errors.New("this project name is invalid, a project name cannot contain spaces. Try using '-' instead")
