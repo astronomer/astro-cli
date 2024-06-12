ac597e6f672caaf7118df1c4585cb17b270e0d32
TJaniF <tamara.fingerlin@astronomer.io>
2024-06-12 21:32:24 +0200

    correct docs link

--- airflow/include/exampledag.py
+++ airflow/include/exampledag.py
@@ -15,7 +15,7 @@ depending on how many Astronauts are in space, and the DAG will adjust
 accordingly each time it runs.
 
 For more explanation and getting started instructions, see our Write your
-first DAG tutorial: https://docs.astronomer.io/learn/get-started-with-airflow
+first DAG tutorial: https://astronomer.io/docs/learn/get-started-with-airflow
 
 ![Picture of the ISS](https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2010/02/space_station_over_earth/10293696-3-eng-GB/Space_Station_over_Earth_card_full.jpg)
 """
