e0b12fb32013062627076376fe4a14cd72300e88
Jeremy Beard <jeremy.beard@astronomer.io>
2024-12-11 14:51:58 +1100

    Fixes

--- cmd/root.go
+++ cmd/root.go
@@ -50,44 +50,58 @@ const (
 
 // NewRootCmd adds all of the primary commands for the cli
 func NewRootCmd() *cobra.Command {
-	var err error
 	httpClient := houston.NewHTTPClient()
 	houstonClient = houston.NewClient(httpClient)
-
 	airflowClient := airflowclient.NewAirflowClient(httputil.NewHTTPClient())
 	astroCoreClient := astrocore.NewCoreClient(httputil.NewHTTPClient())
 	astroCoreIamClient := astroiamcore.NewIamCoreClient(httputil.NewHTTPClient())
 	platformCoreClient := astroplatformcore.NewPlatformCoreClient(httputil.NewHTTPClient())
 
-	ctx := cloudPlatform
+	ctx, isCloudCtx := initializeContext()
+
+	RootPersistentPreRunE = createRootPersistentPreRunE(isCloudCtx, platformCoreClient, astroCoreClient)
+
+	rootCmd := createRootCommand()
+
+	addCommands(rootCmd, isCloudCtx, astroCoreClient, platformCoreClient, airflowClient, astroCoreIamClient, os.Stdout)
+
+	rootCmd.SetHelpTemplate(getResourcesHelpTemplate(houstonVersion, ctx))
+	rootCmd.PersistentFlags().StringVarP(&verboseLevel, "verbosity", "", logrus.WarnLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
+
+	return rootCmd
+}
+
+func initializeContext() (string, bool) {
 	isCloudCtx := context.IsCloudContext()
-	if !isCloudCtx {
+	var ctx string
+	if isCloudCtx {
+		ctx = cloudPlatform
+	} else {
 		ctx = softwarePlatform
+		var err error
 		houstonVersion, err = houstonClient.GetPlatformVersion(nil)
 		if err != nil {
 			softwareCmd.InitDebugLogs = append(softwareCmd.InitDebugLogs, fmt.Sprintf("Unable to get Houston version: %s", err.Error()))
 		}
 	}
+	return ctx, isCloudCtx
+}
 
-	RootPersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		// common PersistentPreRunE component between software & cloud
-		// setting up log verbosity and dumping debug logs collected during CLI-initialization
+func createRootPersistentPreRunE(isCloudCtx bool, platformCoreClient *astroplatformcore.ClientWithResponses, astroCoreClient *astrocore.ClientWithResponses) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
 		if err := softwareCmd.SetUpLogs(os.Stdout, verboseLevel); err != nil {
 			return err
 		}
-		// Check for latest version
 		if config.CFG.UpgradeMessage.GetBool() {
-			// create github client with 3 second timeout, setting an aggressive timeout since its not mandatory to get a response in each command execution
 			githubClient := github.NewClient(&http.Client{Timeout: 3 * time.Second})
-			// compare current version to latest
-			err = version.CompareVersions(githubClient, "astronomer", "astro-cli")
+			err := version.CompareVersions(githubClient, "astronomer", "astro-cli")
 			if err != nil {
 				softwareCmd.InitDebugLogs = append(softwareCmd.InitDebugLogs, "Error comparing CLI versions: "+err.Error())
 			}
 		}
 		if isCloudCtx {
 			if shouldDoCloudAuthSetup(cmd) {
-				err = cloudCmd.AuthSetup(cmd, platformCoreClient, astroCoreClient)
+				err := cloudCmd.AuthSetup(cmd, platformCoreClient, astroCoreClient)
 				if err != nil {
 					if strings.Contains(err.Error(), "token is invalid or malformed") {
 						return errors.New("API Token is invalid or malformed") //nolint
@@ -104,8 +118,10 @@ func NewRootCmd() *cobra.Command {
 		softwareCmd.PrintDebugLogs()
 		return nil
 	}
+}
 
-	rootCmd := &cobra.Command{
+func createRootCommand() *cobra.Command {
+	return &cobra.Command{
 		Use:   "astro",
 		Short: "Run Apache Airflow locally and interact with Astronomer",
 		Long: `
@@ -113,43 +129,40 @@ func NewRootCmd() *cobra.Command {
 /_______/\ /_____/\ /________/\/_____/\  /_____/\           /_____/\ /_/\      /_______/\
 \::: _  \ \\::::_\/_\__.::.__\/\:::_ \ \ \:::_ \ \   _______\:::__\/ \:\ \     \__.::._\/
  \::(_)  \ \\:\/___/\  \::\ \   \:(_) ) )_\:\ \ \ \ /______/\\:\ \  __\:\ \       \::\ \
-  \:: __  \ \\_::._\:\  \::\ \   \: __ '\ \\:\ \ \ \\__::::\/ \:\ \/_/\\:\ \____  _\::\ \__
-   \:.\ \  \ \ /____\:\  \::\ \   \ \ '\ \ \\:\_\ \ \          \:\_\ \ \\:\/___/\/__\::\__/\
-    \__\/\__\/ \_____\/   \__\/    \_\/ \_\/ \_____\/           \_____\/ \_____\/\________\/
+	\:: __  \ \\_::._\:\  \::\ \   \: __ '\ \\:\ \ \ \\__::::\/ \:\ \/_/\\:\ \____  _\::\ \__
+	 \:.\ \  \ \ /____\:\  \::\ \   \ \ '\ \ \\:\_\ \ \          \:\_\ \ \\:\/___/\/__\::\__/\
+	\__\/\__\/ \_____\/   \__\/    \_\/ \_\/ \_____\/           \_____\/ \_____\/\________\/
 
 Welcome to the Astro CLI, the modern command line interface for data orchestration. You can use it for Astro, Astronomer Software, or Local Development.`,
 		PersistentPreRunE: RootPersistentPreRunE,
 	}
+}
 
+func addCommands(rootCmd *cobra.Command, isCloudCtx bool, astroCoreClient *astrocore.ClientWithResponses, platformCoreClient *astroplatformcore.ClientWithResponses, airflowClient *airflowclient.HTTPClient, astroCoreIamClient *astroiamcore.ClientWithResponses, out *os.File) {
 	rootCmd.AddCommand(
-		newLoginCommand(astroCoreClient, platformCoreClient, os.Stdout),
-		newLogoutCommand(os.Stdout),
+		newLoginCommand(astroCoreClient, platformCoreClient, out),
+		newLogoutCommand(out),
 		newVersionCommand(),
 		newDevRootCmd(platformCoreClient, astroCoreClient),
-		newContextCmd(os.Stdout),
-		newConfigRootCmd(os.Stdout),
+		newContextCmd(out),
+		newConfigRootCmd(out),
 		newRunCommand(),
 	)
 
-	if context.IsCloudContext() { // Include all the commands to be exposed for cloud users
+	if isCloudCtx {
 		rootCmd.AddCommand(
-			cloudCmd.AddCmds(platformCoreClient, astroCoreClient, airflowClient, astroCoreIamClient, os.Stdout)...,
+			cloudCmd.AddCmds(platformCoreClient, astroCoreClient, airflowClient, astroCoreIamClient, out)...,
 		)
-	} else { // Include all the commands to be exposed for software users
+	} else {
 		rootCmd.AddCommand(
-			softwareCmd.AddCmds(houstonClient, os.Stdout)...,
+			softwareCmd.AddCmds(houstonClient, out)...,
 		)
 		softwareCmd.VersionMatchCmds(rootCmd, []string{"astro"})
 	}
 
-	rootCmd.AddCommand( // include all the commands for interacting with the registry
-		registry.AddCmds(os.Stdout)...,
+	rootCmd.AddCommand(
+		registry.AddCmds(out)...,
 	)
-
-	rootCmd.SetHelpTemplate(getResourcesHelpTemplate(houstonVersion, ctx))
-	rootCmd.PersistentFlags().StringVarP(&verboseLevel, "verbosity", "", logrus.WarnLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
-
-	return rootCmd
 }
 
 func getResourcesHelpTemplate(houstonVersion, ctx string) string {
