3002d26adcc11927dbf211de4368aa4e862a23f5
Jake Ferriero <jtferriero@gmail.com>
2025-07-03 10:17:47 -0700

    Fix standard deployment worker queue updates (#1889)

--- cloud/deployment/deployment_test.go
+++ cloud/deployment/deployment_test.go
@@ -1740,6 +1740,47 @@ func (s *Suite) TestUpdate() { //nolint
 		s.NoError(err)
 	})
 
+	s.Run("successfully update worker queues with standard type", func() {
+		mockCoreClient.On("GetDeploymentOptionsWithResponse", mock.Anything, mock.Anything, mock.Anything).Return(&GetDeploymentOptionsResponseOK, nil).Times(2)
+		mockPlatformCoreClient.On("UpdateDeploymentWithResponse", mock.Anything, mock.Anything, mock.Anything, mock.MatchedBy(func(i astroplatformcore.UpdateDeploymentRequest) bool {
+			input, _ := i.AsUpdateStandardDeploymentRequest()
+			return input.WorkerQueues != nil && len(*input.WorkerQueues) == 1 && (*input.WorkerQueues)[0].Name == "worker-name"
+		})).Return(&mockUpdateDeploymentResponse, nil).Times(2)
+		mockPlatformCoreClient.On("ListDeploymentsWithResponse", mock.Anything, mock.Anything, mock.Anything).Return(&mockListDeploymentsResponse, nil).Times(2)
+		mockPlatformCoreClient.On("GetDeploymentWithResponse", mock.Anything, mock.Anything, mock.Anything).Return(&deploymentResponse, nil).Times(2)
+
+		// change type to standard
+		deploymentResponse.JSON200.Type = &standardType
+		deploymentResponse.JSON200.WorkerQueues = &[]astroplatformcore.WorkerQueue{
+			{
+				AstroMachine:      &astroMachine,
+				Id:                "queue-id",
+				IsDefault:         true,
+				MaxWorkerCount:    10,
+				MinWorkerCount:    1,
+				Name:              "worker-name",
+				WorkerConcurrency: 20,
+				NodePoolId:        &nodeID,
+			},
+		}
+
+		// mock os.Stdin
+		// Mock user input for deployment name
+		defer testUtil.MockUserInput(s.T(), "1")()
+
+		// success with standard type with name
+		err := Update("", "test", ws, "", "", "enable", CeleryExecutor, "medium", "disable", "disable", "disable", "", "", "2CPU", "2Gi", "", 0, 0, workerQueueRequest, hybridQueueList, newEnvironmentVariables, nil, nil, nil, false, mockCoreClient, mockPlatformCoreClient)
+		s.NoError(err)
+
+		// mock os.Stdin
+		// Mock user input for deployment name
+		defer testUtil.MockUserInput(s.T(), "1")()
+
+		// success with standard type
+		err = Update("", "", ws, "", "test-1", "enable", CeleryExecutor, "medium", "enable", "disable", "disable", "", "", "2CPU", "2Gi", "", 0, 0, nil, hybridQueueList, newEnvironmentVariables, nil, nil, nil, false, mockCoreClient, mockPlatformCoreClient)
+		s.NoError(err)
+	})
+
 	s.Run("successfully update worker queues", func() {
 		mockCoreClient.On("GetDeploymentOptionsWithResponse", mock.Anything, mock.Anything, mock.Anything).Return(&GetDeploymentOptionsResponseOK, nil).Times(2)
 		mockPlatformCoreClient.On("UpdateDeploymentWithResponse", mock.Anything, mock.Anything, mock.Anything, mock.MatchedBy(func(i astroplatformcore.UpdateDeploymentRequest) bool {
