c7702d3000a15b1e23f80e207de5157e7d408a5e
Cole Heflin <cvheflin@gmail.com>
2025-09-03 15:45:00 -0700

    feat: enhance error messaging for environment secrets fetching permission

--- cloud/environment/environment_test.go
+++ cloud/environment/environment_test.go
@@ -1,6 +1,8 @@
 package environment
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -115,4 +117,83 @@ func (s *Suite) TestListConnections() {
 
 		mockClient.AssertExpectations(s.T())
 	})
+
+	s.Run("List connections handles secrets fetching error from listEnvironmentObjects", func() {
+		// This test focuses on verifying that the error detection logic works
+		// The integration will be tested end-to-end manually
+		secretsError := errors.New("showSecrets on organization with id someOrganization is not allowed")
+		result := isSecretsFetchingNotAllowedError(secretsError)
+		s.True(result)
+	})
+}
+
+func (s *Suite) TestIsSecretsFetchingNotAllowedError() {
+	testCases := []struct {
+		name        string
+		err         error
+		expectedResult bool
+	}{
+		{
+			"exact match", 
+			errors.New("showSecrets on organization with id clt4farh34gm801n753x80fo8 is not allowed"),
+			true,
+		},
+		{
+			"case insensitive match", 
+			errors.New("ShowSecrets on ORGANIZATION with id someId IS NOT ALLOWED"),
+			true,
+		},
+		{
+			"wrapped error with secrets error", 
+			fmt.Errorf("API error: %w", errors.New("showSecrets on organization with id test is not allowed")),
+			true,
+		},
+		{
+			"different organization error", 
+			errors.New("organization with id test not found"),
+			false,
+		},
+		{
+			"different secrets error", 
+			errors.New("secrets access denied"),
+			false,
+		},
+		{
+			"regular error", 
+			errors.New("network timeout"),
+			false,
+		},
+		{
+			"nil error", 
+			nil,
+			false,
+		},
+		{
+			"empty error", 
+			errors.New(""),
+			false,
+		},
+		{
+			"partial match - missing showSecrets", 
+			errors.New("organization with id test is not allowed"),
+			false,
+		},
+		{
+			"partial match - missing organization", 
+			errors.New("showSecrets with id test is not allowed"),
+			false,
+		},
+		{
+			"partial match - missing not allowed", 
+			errors.New("showSecrets on organization with id test is allowed"),
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			result := isSecretsFetchingNotAllowedError(tc.err)
+			s.Equal(tc.expectedResult, result, "Expected isSecretsFetchingNotAllowedError(%v) to be %v", tc.err, tc.expectedResult)
+		})
+	}
 }
