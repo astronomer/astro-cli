1d8af29ddee0ad1f1a9ee79c423ac3eb34d14876
Sean Muth <seanmuth@users.noreply.github.com>
2026-05-06 12:56:24 -0500

    fix(deploy): skip dag-deploy check on Remote Execution deployments (#2122)

--- cloud/deploy/deploy.go
+++ cloud/deploy/deploy.go
@@ -235,7 +235,7 @@ func Deploy(deployInput InputDeploy, platformCoreClient astroplatformcore.CoreCl
 		return nil
 	}
 
-	if deployInput.Image {
+	if deployInput.Image && !deployInfo.isRemoteExecutionEnabled {
 		if !deployInfo.dagDeployEnabled {
 			return fmt.Errorf(enableDagDeployMsg, deployInfo.deploymentID) //nolint
 		}
